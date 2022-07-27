95579f1d6003ef37e9313951882ca6fa67c99c00
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-07-27 13:47:35 -0700

    use next link

--- frontend/src/components/Sidebar/SideNavLink.tsx
+++ frontend/src/components/Sidebar/SideNavLink.tsx
@@ -8,6 +8,7 @@ import {
   Tooltip,
   useColorModeValue,
 } from "@chakra-ui/react";
+import NextLink from "next/link";
 import React from "react";
 import {
   sideNavDestinationToHref,
@@ -27,50 +28,51 @@ const SideNavLink: React.FC<SideNavLinkProps> = React.memo(
       ? "rgb(101, 138, 216)"
       : useColorModeValue("rgb(163, 165, 170)", "rgb(98, 100, 116)");
     return (
-      <Link
-        display="block"
-        py={3}
-        px={4}
-        borderRadius="lg"
-        transition="all 0.15s"
-        fontWeight="medium"
-        lineHeight="1.5rem"
-        aria-current={isActive ? "page" : undefined}
-        _hover={{
-          bg: useColorModeValue("rgb(240, 240, 242)", "rgb(25, 28, 35)"),
-        }}
-        _activeLink={{
-          color: useColorModeValue("black", "white"),
-        }}
-        {...rest}
-        href={sideNavDestinationToHref(destination)}
-      >
-        <Box display={{ xl: "unset", base: "none" }}>
-          <HStack spacing={4} alignItems="center">
-            <Icon
-              as={sideNavDestinationToIcon(destination)}
-              boxSize="20px"
-              color={iconColor}
-            />
-            <Text>{sideNavDestinationToLabel(destination)}</Text>
-          </HStack>
-        </Box>
-        <Box display={{ xl: "none", base: "unset" }}>
-          <Tooltip
-            hasArrow
-            placement="left"
-            label={sideNavDestinationToLabel(destination)}
-          >
-            <span>
+      <NextLink href={sideNavDestinationToHref(destination)}>
+        <Link
+          display="block"
+          py={3}
+          px={4}
+          borderRadius="lg"
+          transition="all 0.15s"
+          fontWeight="medium"
+          lineHeight="1.5rem"
+          aria-current={isActive ? "page" : undefined}
+          _hover={{
+            bg: useColorModeValue("rgb(240, 240, 242)", "rgb(25, 28, 35)"),
+          }}
+          _activeLink={{
+            color: useColorModeValue("black", "white"),
+          }}
+          {...rest}
+        >
+          <Box display={{ xl: "unset", base: "none" }}>
+            <HStack spacing={4} alignItems="center">
               <Icon
                 as={sideNavDestinationToIcon(destination)}
                 boxSize="20px"
                 color={iconColor}
               />
-            </span>
-          </Tooltip>
-        </Box>
-      </Link>
+              <Text>{sideNavDestinationToLabel(destination)}</Text>
+            </HStack>
+          </Box>
+          <Box display={{ xl: "none", base: "unset" }}>
+            <Tooltip
+              hasArrow
+              placement="left"
+              label={sideNavDestinationToLabel(destination)}
+            >
+              <span>
+                <Icon
+                  as={sideNavDestinationToIcon(destination)}
+                  boxSize="20px"
+                  color={iconColor}
+                />
+              </span>
+            </Tooltip>
+          </Box>
+        </Link>
+      </NextLink>
     );
   }
 );
