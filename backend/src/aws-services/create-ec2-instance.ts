0ffcb290d757977a74352db688808158589b0f39
Ninad Sinha <ninadsinhaa@gmail.com>
2022-08-06 23:34:00 +0530

    describe instance details and get all instances meeting specifications

--- backend/src/aws-services/create-ec2-instance.ts
+++ backend/src/aws-services/create-ec2-instance.ts
@@ -3,18 +3,31 @@ import {
   DescribeImagesCommand,
   DescribeImagesCommandInput,
   DescribeImagesCommandOutput,
-  Filter,
   EC2ClientConfig,
+  RunInstancesCommand,
+  RunInstancesCommandInput,
+  RunInstancesCommandOutput,
+  DescribeInstanceTypesCommand,
+  DescribeInstanceTypesCommandInput,
+  GetInstanceTypesFromInstanceRequirementsCommand,
+  GetInstanceTypesFromInstanceRequirementsCommandInput,
+  InstanceTypeInfoFromInstanceRequirements,
+  Image,
 } from "@aws-sdk/client-ec2";
 import { Config } from "aws-sdk";
 import { response } from "express";
-// Set the AWS Region.
-// TODO : Get info from params
+
+interface MachineSpecifications {
+  minCpu: number;
+  maxCpu: number;
+  minMem: number;
+  maxMem?: number;
+}
 
 async function get_all_images(
   img_names: Array<string>,
   config: EC2ClientConfig
-): Promise<DescribeImagesCommandOutput["Images"]> {
+): Promise<Array<Image>> {
   // Create anAmazon EC2 service client object.
   const ec2Client = new EC2Client(config);
   const input: DescribeImagesCommandInput = {
@@ -48,4 +61,35 @@ async function get_latest_image(
   let resp = (await get_all_images(img_names, config)).pop();
   console.log(resp);
   return resp;
+}
+
+async function get_valid_types(
+  config: EC2ClientConfig,
+  image: Image,
+  specs: MachineSpecifications
+): Promise<Array<InstanceTypeInfoFromInstanceRequirements>> {
+  let client = new EC2Client(config);
+  let command = new GetInstanceTypesFromInstanceRequirementsCommand({
+    ArchitectureTypes: ["x86_64"],
+    VirtualizationTypes: [image.VirtualizationType],
+    InstanceRequirements: {
+      VCpuCount: { Min: specs.minCpu, Max: specs.maxCpu },
+      MemoryMiB: {
+        Min: specs.minMem * 1024,
+        Max: specs.maxMem ? specs.maxMem * 1024 : null,
+      },
+      InstanceGenerations: ["current"],
+    },
+  } as GetInstanceTypesFromInstanceRequirementsCommandInput);
+  let resp = await client.send(command);
+  return resp.InstanceTypes;
+}
+
+async function describe_type(config: EC2ClientConfig, InstanceType: string) {
+  let client = new EC2Client(config);
+  let command = new DescribeInstanceTypesCommand({
+    InstanceTypes: [InstanceType],
+  } as DescribeInstanceTypesCommandInput);
+  let resp = await client.send(command);
+  return resp.InstanceTypes[0];
 }
