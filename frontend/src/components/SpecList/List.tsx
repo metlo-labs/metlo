782dcc487f4f8b0fa45cb62f9fe42623d000e81e
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-08-07 15:30:53 -0700

    more work on spec list

--- frontend/src/components/SpecList/List.tsx
+++ frontend/src/components/SpecList/List.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import yaml from "js-yaml";
 import { useColorMode } from "@chakra-ui/react";
 import DataTable, { TableColumn } from "react-data-table-component";
 import { getCustomStyles, rowStyles } from "../utils/TableUtils";
 import { OpenApiSpec } from "@common/types";
+import { SpecExtension } from "@common/enums";
 
 interface SpecListProps {
   apiSpecs: OpenApiSpec[];
 }
+
+const getHostsFromSpec = (spec: OpenApiSpec): string[] => {
+  let parsedSpec = null;
+  if ((spec.extension = SpecExtension.JSON)) {
+    parsedSpec = JSON.parse(spec.spec);
+  } else if ((spec.extension = SpecExtension.YAML)) {
+    parsedSpec = yaml.load(spec.spec);
+  }
+  if (parsedSpec) {
+    return parsedSpec.servers.map((e: any) => e.url);
+  }
+};
 
 const SpecList: React.FC<SpecListProps> = React.memo(({ apiSpecs }) => {
   const colorMode = useColorMode();
@@ -16,6 +30,12 @@ const SpecList: React.FC<SpecListProps> = React.memo(({ apiSpecs }) => {
       sortable: true,
       selector: (row: OpenApiSpec) => row.name || "",
       id: "name",
+    },
+    {
+      name: "Hosts",
+      sortable: true,
+      selector: (row: OpenApiSpec) => getHostsFromSpec(row).join(", ") || "",
+      id: "hosts",
     },
   ];
   return (
