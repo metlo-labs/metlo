924d6e64503e15acaab8da37b0ccacd16c4b1da7
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-08-06 17:04:36 -0700

    use env var for backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   async rewrites() {
+    var backendURL = process.env.BACKEND_URL || 'http://localhost:8080'
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: `${backendURL}/api/:path*`,
       },
     ];
   },
