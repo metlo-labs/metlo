e0bc32537673df61a0d46bd6df51d0424c55b5f7
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-08-11 23:43:47 -0700

    add new test data producer

--- ingestors/test_data/producers/__init__.py
+++ ingestors/test_data/producers/__init__.py
@@ -1,3 +1,4 @@
+from .ecommerce_make_product import EcommerceMakeProductProducer
 from producers.ecommerce_make_cart import EcommerceMakeCartProducer
 from producers.ecommerce_add_product_to_cart import EcommerceAddProductToCartProducer
 
@@ -5,4 +6,5 @@ from producers.ecommerce_add_product_to_cart import EcommerceAddProductToCartPro
 PRODUCER_CLS_MAP = {
     "ecommerce_add_product_to_cart": EcommerceAddProductToCartProducer,
     "ecommerce_make_cart": EcommerceMakeCartProducer,
+    "ecommerce_make_product": EcommerceMakeProductProducer,
 }
---
+++ ingestors/test_data/producers/ecommerce_make_product.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from typing import List
+from uuid import uuid4
+import json
+from random import choice, randint
+from faker import Faker
+from faker.providers import internet
+
+fake = Faker()
+fake.add_provider(internet)
+
+
+from producers.base import BaseProducer
+
+
+class EcommerceMakeProductProducer(BaseProducer):
+
+    avg_emit_delta = timedelta(minutes=5)
+
+    def get_data_points_helper(self) -> dict:
+        resp_body = {
+            "ok": True,
+            "cart_uuid": str(uuid4()),
+            "msg": "New Product Created..."
+        }
+        req_body = {
+            "name": "Test Product",
+            "price": randint(100, 1000),
+        }
+        return {
+            "request": {
+                "url": {
+                    "host": "test-ecommerce.metlo.com",
+                    "path": "/product",
+                    "parameters": []
+                },
+                "method": "POST",
+                "body": "",
+            },
+            "response": {
+                "status": 200,
+                "headers": [
+                    {
+                        "name": "content-type",
+                        "value": "application/json; charset=utf-8",
+                    },
+                ],
+                "body": json.dumps(resp_body),
+            },
+            "meta": {
+                "environment": "production",
+                "incoming": True,
+                "source": fake.ipv4(),
+                "sourcePort": choice(range(10000, 20000)),
+                "destination": "76.47.25.189",
+                "destinationPort": 443,
+            },
+        }
+
+    def get_data_points(self, time: datetime) -> List[dict]:
+        return [self.get_data_points_helper()]
