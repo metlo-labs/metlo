b5f8cc9500d1e6a2da8f4b1fe6da24659db2600e
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-08-13 17:58:45 -0700

    add favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,9 +5,26 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+        </Head>
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
