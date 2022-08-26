a66fe88786bbb5e9c98abd330f79ea64c441f7be
Shridhar Sukhani <sukha014@umn.edu>
2022-08-26 12:57:49 -0700

    Format ingestors/suricata/

--- ingestors/suricata/index.ts
+++ ingestors/suricata/index.ts
@@ -1,73 +1,73 @@
-import net from "net";
-import fs from "fs";
-import process from "process";
-import { conns, EVENTS, ALERT } from "./interface";
-import { program } from "commander";
-import { prepareResponse, compileHost, pushAlert } from "./utils";
+import net from "net"
+import fs from "fs"
+import process from "process"
+import { conns, EVENTS, ALERT } from "./interface"
+import { program } from "commander"
+import { prepareResponse, compileHost, pushAlert } from "./utils"
 
-var server: net.Server;
-var connections: Record<number, net.Socket> = {};
-var http_meta: Record<string, conns> = {};
+var server: net.Server
+var connections: Record<number, net.Socket> = {}
+var http_meta: Record<string, conns> = {}
 
-var url = "";
+var url = ""
 // Our socket
-var SOCKETFILE = "";
+var SOCKETFILE = ""
 
 function createServer(socket: string) {
-  console.log("Creating server.");
+  console.log("Creating server.")
   var server = net
     .createServer(function (stream) {
-      console.log("Connection acknowledged.");
+      console.log("Connection acknowledged.")
 
       // Store all connections so we can terminate them if the server closes.
       // An object is better than an array for these.
-      var self = Date.now();
-      connections[self] = stream;
+      var self = Date.now()
+      connections[self] = stream
       stream.on("end", function () {
-        console.log("Client disconnected.");
-        delete connections[self];
-      });
+        console.log("Client disconnected.")
+        delete connections[self]
+      })
 
       // Messages are buffers. use toString
       stream.on("data", function (msg) {
         msg
           .toString()
           .split("\n")
-          .filter((_msg) => !!_msg)
+          .filter(_msg => !!_msg)
           .forEach((_msg, i, a) => {
             try {
-              const jsonmsg = JSON.parse(_msg);
+              const jsonmsg = JSON.parse(_msg)
               if (EVENTS.HTTP === (jsonmsg["event_type"] as string)) {
-                compileHost(jsonmsg, http_meta);
+                compileHost(jsonmsg, http_meta)
               }
               if (EVENTS.ALERT === (jsonmsg["event_type"] as string)) {
                 // compileAlert(jsonmsg);
-                const alert: ALERT = jsonmsg;
+                const alert: ALERT = jsonmsg
                 // Get first metadata for the given connection.
-                let meta = http_meta[alert.flow_id].metas.shift();
+                let meta = http_meta[alert.flow_id].metas.shift()
                 if (meta) {
-                  let resp = prepareResponse(alert, meta);
-                  pushAlert(resp, url);
+                  let resp = prepareResponse(alert, meta)
+                  pushAlert(resp, url)
                 }
               }
             } catch (err) {
               console.log(
-                `///////////////////     ERROR      ///////////////////`
-              );
-              console.log(err);
+                `///////////////////     ERROR      ///////////////////`,
+              )
+              console.log(err)
               console.log(
-                `///////////////////     MESSAGE      ///////////////////`
-              );
-              console.log(_msg);
+                `///////////////////     MESSAGE      ///////////////////`,
+              )
+              console.log(_msg)
             }
-          });
-      });
+          })
+      })
     })
     .listen(socket)
     .on("connection", function (socket) {
-      console.log("Client connected.");
-    });
-  return server;
+      console.log("Client connected.")
+    })
+  return server
 }
 
 // check for failed cleanup
@@ -75,54 +75,54 @@ function main() {
   // Set cli options
   program
     .name("AWS-Suricata Ingestor")
-    .description("Basic CLI app to ingest data from suricata on AWS");
+    .description("Basic CLI app to ingest data from suricata on AWS")
   program
     .requiredOption("-u, --url <url>", "URL for the webhook destination")
-    .requiredOption("-s, --socket <socket_path>", "Socket file path");
-  program.parse(process.argv);
-  let options = program.opts();
+    .requiredOption("-s, --socket <socket_path>", "Socket file path")
+  program.parse(process.argv)
+  let options = program.opts()
   if (new URL(options.url)) {
-    url = options.url;
-    SOCKETFILE = options.socket;
+    url = options.url
+    SOCKETFILE = options.socket
   }
 
-  console.info("Socket: %s \n  Process: %s", SOCKETFILE, process.pid);
-  console.log("Checking for leftover socket.");
+  console.info("Socket: %s \n  Process: %s", SOCKETFILE, process.pid)
+  console.log("Checking for leftover socket.")
   fs.stat(SOCKETFILE, function (err, stats) {
     if (err) {
       // start server
-      console.log("No leftover socket found.");
-      server = createServer(SOCKETFILE);
-      return;
+      console.log("No leftover socket found.")
+      server = createServer(SOCKETFILE)
+      return
     }
     // remove file then start server
-    console.log("Removing leftover socket.");
+    console.log("Removing leftover socket.")
     fs.unlink(SOCKETFILE, function (err) {
       if (err) {
         // This should never happen.
-        console.error(err);
-        process.exit(0);
+        console.error(err)
+        process.exit(0)
       }
-      server = createServer(SOCKETFILE);
-      return;
-    });
-  });
+      server = createServer(SOCKETFILE)
+      return
+    })
+  })
 
   // close all connections when the user does CTRL-C
   function cleanup() {
-    console.log(`\nClosing Connections\n`);
+    console.log(`\nClosing Connections\n`)
     if (Object.keys(connections).length) {
-      Object.keys(connections).forEach((client) => {
-        connections[parseInt(client)].end();
-      });
-      connections = [];
+      Object.keys(connections).forEach(client => {
+        connections[parseInt(client)].end()
+      })
+      connections = []
     }
-    console.log(`\nTerminating server\n`);
-    server.close();
-    console.log(`\nClosing Process\n`);
-    process.exit(0);
+    console.log(`\nTerminating server\n`)
+    server.close()
+    console.log(`\nClosing Process\n`)
+    process.exit(0)
   }
-  process.on("SIGINT", cleanup);
+  process.on("SIGINT", cleanup)
 }
 
-main();
+main()
--- ingestors/suricata/interface.ts
+++ ingestors/suricata/interface.ts
@@ -1,9 +1,9 @@
 export interface conns {
-  flowId: number;
+  flowId: number
   metas: Array<{
-    timestamp: string;
-    metadata: HOST;
-  }>;
+    timestamp: string
+    metadata: HOST
+  }>
 }
 
 export enum EVENTS {
@@ -12,112 +12,112 @@ export enum EVENTS {
 }
 
 export interface HEADERS {
-  name: string;
-  value: string;
+  name: string
+  value: string
 }
 
 export interface COMMON {
-  timestamp: string;
-  flow_id: number;
-  in_iface: string;
-  event_type: string;
-  src_ip: string;
-  src_port: number;
-  dest_ip: string;
-  dest_port: number;
-  proto: string;
-  community_id: string;
-  tx_id: number;
+  timestamp: string
+  flow_id: number
+  in_iface: string
+  event_type: string
+  src_ip: string
+  src_port: number
+  dest_ip: string
+  dest_port: number
+  proto: string
+  community_id: string
+  tx_id: number
 }
 
 export interface HOST extends COMMON {
-  http: Object;
-  request_headers: Array<HEADERS>;
-  response_headers: Array<HEADERS>;
+  http: Object
+  request_headers: Array<HEADERS>
+  response_headers: Array<HEADERS>
 }
 
 export interface ALERT extends COMMON {
   alert: {
-    action: string;
-    gid: number;
-    signature_id: number;
-    rev: number;
-    signature: string;
-    category: string;
-    severity: number;
-  };
+    action: string
+    gid: number
+    signature_id: number
+    rev: number
+    signature: string
+    category: string
+    severity: number
+  }
   tunnel: {
-    src_ip: string;
-    src_port: number;
-    dest_ip: string;
-    dest_port: number;
-    proto: string;
-    depth: number;
-  };
+    src_ip: string
+    src_port: number
+    dest_ip: string
+    dest_port: number
+    proto: string
+    depth: number
+  }
   http: {
-    hostname: string;
-    http_port: number;
-    url: string;
-    http_user_agent?: string;
-    http_content_type: string;
-    http_method: string;
-    protocol: string;
-    status: number;
-    length: number;
-    http_request_body_printable: string;
-    http_response_body_printable: string;
-    http_request_body: string;
-    http_response_body: string;
-  };
+    hostname: string
+    http_port: number
+    url: string
+    http_user_agent?: string
+    http_content_type: string
+    http_method: string
+    protocol: string
+    status: number
+    length: number
+    http_request_body_printable: string
+    http_response_body_printable: string
+    http_request_body: string
+    http_response_body: string
+  }
   files: [
     {
-      filename: string;
-      sid: Array<any>;
-      gaps: boolean;
-      state: string;
-      stored: boolean;
-      size: number;
-      tx_id: number;
-    }
-  ];
-  app_proto: string;
+      filename: string
+      sid: Array<any>
+      gaps: boolean
+      state: string
+      stored: boolean
+      size: number
+      tx_id: number
+    },
+  ]
+  app_proto: string
   flow: {
-    pkts_toserver: number;
-    pkts_toclient: number;
-    bytes_toserver: number;
-    bytes_toclient: number;
-    start: string;
-  };
-  payload: string;
-  payload_printable: string;
-  stream: number;
-  packet: string;
-  packet_info?: { linktype: number };
+    pkts_toserver: number
+    pkts_toclient: number
+    bytes_toserver: number
+    bytes_toclient: number
+    start: string
+  }
+  payload: string
+  payload_printable: string
+  stream: number
+  packet: string
+  packet_info?: { linktype: number }
 }
 
 export interface RESPONSE {
   request: {
     url: {
-      base_url: string;
-      path: string;
-      parameters: Array<HEADERS>;
-    };
-    headers: Array<HEADERS>;
-    body?: string;
-    method: string;
-  };
+      base_url: string
+      path: string
+      parameters: Array<HEADERS>
+    }
+    headers: Array<HEADERS>
+    body?: string
+    method: string
+  }
   response: {
-    url: string;
-    status: number;
-    headers: Array<HEADERS>;
-    body?: string;
-  };
+    url: string
+    status: number
+    headers: Array<HEADERS>
+    body?: string
+  }
   meta: {
-    environment: "production";
-    incoming: true;
-    source: string;
-    source_port: number;
-    destination: string;
-    destination_port: number;
-  };
+    environment: "production"
+    incoming: true
+    source: string
+    source_port: number
+    destination: string
+    destination_port: number
+  }
 }
