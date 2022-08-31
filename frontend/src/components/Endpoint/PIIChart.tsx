ef1a554fd48a2b1eb1c1c66bc2d532583d869983
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-08-30 18:57:47 -0700

    (style) PII Chart Labels

--- frontend/src/components/Endpoint/PIIChart.tsx
+++ frontend/src/components/Endpoint/PIIChart.tsx
@@ -1,19 +1,41 @@
 import React from "react"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  ChartOptions,
+} from "chart.js"
 import { LayoutPosition } from "chart.js/types/layout"
-import { Pie } from "react-chartjs-2"
+import { Doughnut } from "react-chartjs-2"
 import { DataField } from "@common/types"
+import { useColorModeValue } from "@chakra-ui/react"
 
 ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register({
+  id: "Pie Chart Center",
+  beforeDraw: function (chart: any) {
+    if (chart.config.options.elements?.center) {
+      const ctx = chart.ctx
+      const centerConfig = chart.config.options.elements.center
+      const lineHeight = 40
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as LayoutPosition,
-    },
+      ctx.textAlign = "center"
+      ctx.textBaseline = "middle"
+      const centerX = (chart.chartArea.left + chart.chartArea.right) / 2
+      let centerY = (chart.chartArea.top + chart.chartArea.bottom) / 2
+      centerY -= lineHeight
+
+      ctx.font = `bold 18px Arial`
+      ctx.fillStyle = centerConfig.headerColor
+      ctx.fillText(centerConfig.header, centerX, centerY)
+      centerY += lineHeight
+      ctx.font = `38px Arial`
+      ctx.fillStyle = centerConfig.textColor
+      ctx.fillText(centerConfig.text, centerX, centerY)
+    }
   },
-}
+})
 
 interface EndpointPIIChartProps {
   piiFields: DataField[]
@@ -21,6 +43,8 @@ interface EndpointPIIChartProps {
 
 const EndpointPIIChart: React.FC<EndpointPIIChartProps> = React.memo(
   ({ piiFields }) => {
+    const headerColor = useColorModeValue("rgb(102, 105, 117)", "rgb(116, 120, 138)")
+    const textColor = useColorModeValue("#000", "#FFF")
     let dataClassToCount = {}
     piiFields
       .map(e => e.dataClasses)
@@ -35,12 +59,12 @@ const EndpointPIIChart: React.FC<EndpointPIIChartProps> = React.memo(
         {
           data: Object.values(dataClassToCount),
           backgroundColor: [
-            "rgba(255, 99, 132, 0.3)",
-            "rgba(54, 162, 235, 0.3)",
-            "rgba(255, 206, 86, 0.3)",
-            "rgba(75, 192, 192, 0.3)",
-            "rgba(153, 102, 255, 0.3)",
-            "rgba(255, 159, 64, 0.3)",
+            "rgba(255, 99, 132, 0.8)",
+            "rgba(54, 162, 235, 0.8)",
+            "rgba(255, 206, 86, 0.8)",
+            "rgba(75, 192, 192, 0.8)",
+            "rgba(153, 102, 255, 0.8)",
+            "rgba(255, 159, 64, 0.8)",
           ],
           borderColor: [
             "rgba(255, 99, 132, 1)",
@@ -54,7 +78,25 @@ const EndpointPIIChart: React.FC<EndpointPIIChartProps> = React.memo(
         },
       ],
     }
-    return <Pie options={options} data={data} />
+    const options = {
+      responsive: true,
+      elements: {
+        center: {
+          headerColor,
+          textColor,
+          header: "Total Fields",
+          text: piiFields.length.toString(),
+        },
+      },
+      cutout: "60%",
+      plugins: {
+        legend: {
+          position: "top" as LayoutPosition,
+        },
+      },
+    } as ChartOptions
+
+    return <Doughnut options={options} data={data} />
   },
 )
 
