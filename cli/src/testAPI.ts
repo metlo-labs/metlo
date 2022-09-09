d86648ddf3bdfbc5e0aaf66d67404cc5e52b9167
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-09-09 02:40:41 -0700

    fill in test api function in cli

--- cli/src/testAPI.ts
+++ cli/src/testAPI.ts
@@ -1,16 +1,27 @@
 import axios from "axios"
+import chalk from "chalk"
 import { getConfig } from "./utils"
 
-import { Test, runTest } from "@metlo/testing"
+import { Test, runTest, TestResult, Result } from "@metlo/testing"
 
 interface testAPIArgs {
   host: string
   target: string
 }
 
-const getTests = async (args: testAPIArgs): Promise<Test[]> => {
+interface APIEndpoint {
+  method: string
+  host: string
+  path: string
+}
+
+interface TestResponse extends Test {
+  apiEndpoint: APIEndpoint
+}
+
+const getTests = async (args: testAPIArgs): Promise<TestResponse[]> => {
   const config = getConfig()
-  const testResponse = await axios.get<Test[]>(
+  const testResponse = await axios.get<TestResponse[]>(
     `${config.metloHost}/api/v1/test/list?hostname=${args.host}`,
   )
   return testResponse.data
@@ -20,7 +31,35 @@ const testAPI = async (args: testAPIArgs) => {
   try {
     const tests = await getTests(args)
     const outputs = await Promise.all(tests.map(runTest))
-    console.log(outputs)
+    const testWithOutputs: [TestResponse, Result[]][] = outputs.map((e, i) => [
+      tests[i],
+      e,
+    ])
+    let headerToFailures = new Map<string, TestResult[]>()
+    testWithOutputs.forEach(([test, results]) => {
+      const name = `${chalk.red(test.name)}\n${test.apiEndpoint.method} ${test.apiEndpoint.path}`
+      const failureResults = results
+        .flatMap(e => e.testResults)
+        .filter(e => !e.success)
+      if (failureResults.length > 0) {
+        let currentFailures = headerToFailures.get(name) || []
+        currentFailures.push(...failureResults)
+        headerToFailures.set(name, currentFailures)
+      }
+    })
+    let output = ""
+    headerToFailures.forEach((testResults, key) => {
+      output += `${key}\n\n`
+      output += testResults.map(e => e.output).join("\n")
+    })
+    if (output.length > 0) {
+      console.log(chalk.red.bold("Some Tests Failed:\n"))
+      console.log(output)
+      process.exit(1)
+    } else {
+      console.log(chalk.green.bold("All tests succeeded!"))
+      process.exit(0)
+    }
   } catch (e) {
     console.error(e.message)
     return
