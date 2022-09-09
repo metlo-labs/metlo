4432014229a2917e14903fd1038ceaa6ea5d238f
Ninad Sinha <ninad@metlo.com>
2022-09-09 14:12:31 +0530

    (feature) Add option to provide host on init to metlo cli

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -4,7 +4,11 @@ import testAPI from "./testAPI"
 
 program.name("metlo").description("Metlo's command line tool.").version("0.0.0")
 
-program.command("init").description("Initialize the Metlo CLI.").action(init)
+program
+  .command("init")
+  .description("Initialize the Metlo CLI.")
+  .argument("-b, --backend_url <string>", "The backend address for Metlo")
+  .action(init)
 program
   .command("test")
   .description("Run Metlo API tests against a specified host.")
--- cli/src/init.ts
+++ cli/src/init.ts
@@ -1,14 +1,41 @@
-import fs from "fs"
+import fs, { writeFileSync } from "fs"
 import os from "os"
 import path from "path"
 import { CREDENTIAL_FILE } from "./utils"
+import readline from "readline"
+import { promisify } from "util"
+import { prompt } from "enquirer"
 
-const init = () => {
+const setup_host = async () => {
+  // Function returns string, is marked as void for some reason.
+  return (
+    await prompt({
+      type: "input",
+      name: "host",
+      message: "Host address of metlo backend :",
+    })
+  )["host"]
+}
+
+const init = async host => {
   const homeDir = os.homedir()
   const creditialFileExists = fs.existsSync(path.join(homeDir, CREDENTIAL_FILE))
   if (creditialFileExists) {
     console.log(
       `Credentials already exist at ~/${CREDENTIAL_FILE}. Are you sure you want to replace them? [Y/n]`,
+    )
+  } else {
+    var backend_host: string
+    console.log(host)
+    // console.log(rest)
+    if (host) {
+      backend_host = host
+    } else {
+      backend_host = await setup_host()
+    }
+    writeFileSync(
+      path.join(homeDir, CREDENTIAL_FILE),
+      `METLO_HOST=${backend_host}`,
     )
   }
 }
