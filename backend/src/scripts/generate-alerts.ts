48af7154a46b52608d018db9a61ccc95c24fec81
Nikhil Shahi <nshahi1998@gmail.com>
2022-09-12 19:59:55 -0500

    (feature) add script for generating alert

---
+++ backend/src/scripts/generate-alerts.ts
@@ -0,0 +1,37 @@
+import yargs from "yargs"
+import { AlertType } from "@common/enums"
+import { ALERT_TYPE_TO_RISK_SCORE } from "@common/maps"
+import { AppDataSource } from "data-source"
+import { Alert } from "models"
+import { DatabaseService } from "services/database"
+
+const generateAlert = async (alertType: string, apiEndpointUuid: string, description: string, context: any) => {
+  try{
+    const newAlert = new Alert()
+    newAlert.apiEndpointUuid = apiEndpointUuid
+    newAlert.type = AlertType[alertType]
+    newAlert.description = description
+    newAlert.riskScore = ALERT_TYPE_TO_RISK_SCORE[AlertType[alertType]]
+    newAlert.context = JSON.parse(context || "{}")
+    await DatabaseService.executeTransactions([[newAlert]], [], false)
+  } catch (err) {
+    console.error(`Error generating new alert from script: ${err}`)
+  }
+}
+
+const main = async () => {
+  const datasource = await AppDataSource.initialize()
+  if (!datasource.isInitialized) {
+    console.error("Couldn't initialize datasource...")
+    return
+  }
+  console.log("AppDataSource Initialized...")
+  const args = yargs.argv
+  const alertType = args["alertType"]
+  const apiEndpointUuid = args["endpointUuid"]
+  const description = args["description"]
+  const context = args["context"]
+  await generateAlert(alertType, apiEndpointUuid, description, context)
+}
+
+main()
