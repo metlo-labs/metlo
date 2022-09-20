992f33d38921205e298832537a8c57b53d5c3e11
Nikhil Shahi <nshahi1998@gmail.com>
2022-09-19 19:11:37 -0500

    add ui tooltip for conversion

--- frontend/src/pages/specs.tsx
+++ frontend/src/pages/specs.tsx
@@ -1,6 +1,14 @@
 import superjson from "superjson"
 import { GetServerSideProps } from "next"
-import { Heading, VStack } from "@chakra-ui/react"
+import { HiOutlineExclamationCircle } from "@react-icons/all-files/hi/HiOutlineExclamationCircle"
+import {
+  Box,
+  Heading,
+  HStack,
+  Icon,
+  VStack,
+  Tooltip,
+} from "@chakra-ui/react"
 import { SideNavLinkDestination } from "components/Sidebar/NavLinkUtils"
 import { SidebarLayoutShell } from "components/SidebarLayoutShell"
 import { ContentContainer } from "components/utils/ContentContainer"
@@ -15,9 +23,25 @@ const Specs = ({ apiSpecs }) => (
   >
     <ContentContainer>
       <VStack w="full" alignItems="flex-start">
-        <Heading fontWeight="medium" size="xl" mb="8">
-          API Specs
-        </Heading>
+        <HStack w="full" mb="8">
+          <Heading fontWeight="medium" size="xl">
+            API Specs
+          </Heading>
+          <Box alignSelf="end">
+            <Tooltip
+              bg="orange.300"
+              placement="right"
+              shouldWrapChildren
+              label="Any Swagger(OpenAPI V2) specifications will be converted to OpenAPI V3 and stored as such."
+            >
+              <Icon
+                as={HiOutlineExclamationCircle}
+                boxSize="30px"
+                color="orange.500"
+              />
+            </Tooltip>
+          </Box>
+        </HStack>
         <SpecList apiSpecs={superjson.parse<OpenApiSpec[]>(apiSpecs)} />
       </VStack>
     </ContentContainer>
