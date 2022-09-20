2f0b11d567bc43c84f07c1b95d8a58c0993dad33
Nikhil Shahi <nshahi1998@gmail.com>
2022-09-19 19:16:32 -0500

    update spec component in endpoint detail

--- frontend/src/pages/specs.tsx
+++ frontend/src/pages/specs.tsx
@@ -1,14 +1,7 @@
 import superjson from "superjson"
 import { GetServerSideProps } from "next"
 import { HiOutlineExclamationCircle } from "@react-icons/all-files/hi/HiOutlineExclamationCircle"
-import {
-  Box,
-  Heading,
-  HStack,
-  Icon,
-  VStack,
-  Tooltip,
-} from "@chakra-ui/react"
+import { Box, Heading, HStack, Icon, VStack, Tooltip } from "@chakra-ui/react"
 import { SideNavLinkDestination } from "components/Sidebar/NavLinkUtils"
 import { SidebarLayoutShell } from "components/SidebarLayoutShell"
 import { ContentContainer } from "components/utils/ContentContainer"
