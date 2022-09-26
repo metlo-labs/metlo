1325bf3458c90ff9a3432c281b9d6bb3d510ca38
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-09-25 19:21:32 -0700

    add shim for enterprise code

---
+++ backend/src/utils/license.ts
@@ -0,0 +1,34 @@
+import cache from "memory-cache"
+
+const hasValidLicenseInner = async (): Promise<boolean> => {
+  const key = process.env.LICENSE_KEY
+  if (!key) {
+    console.log(`No LICENSE_KEY env var specified...`)
+    return false
+  }
+  try {
+    // @ts-ignore
+    const license = await import("@enterprise/license")
+    try {
+      await license.validateLicense(key)
+      return true
+    } catch (e) {
+      console.log(`Couldn't validate license key: ${e.message}`)
+    }
+  } catch {
+    console.log(
+      `Couldn't import enterprise module, contact Metlo for the enterprise package...`,
+    )
+    return false
+  }
+}
+
+export const hasValidLicense = async (): Promise<boolean> => {
+  const cacheRes: boolean | null = cache.get("hasValidLicense")
+  if (cacheRes !== null) {
+    return cacheRes
+  }
+  const valid = await hasValidLicenseInner()
+  cache.put("license", valid, 6 * 60 * 60 * 1000)
+  return valid
+}
