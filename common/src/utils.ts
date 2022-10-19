615da9630336fddfb9aab9739b0b9517d0049091
Nikhil Shahi <nshahi1998@gmail.com>
2022-10-19 15:27:44 -0500

    add optional slashes at path end to regex

--- common/src/utils.ts
+++ common/src/utils.ts
@@ -6,8 +6,5 @@ export const getPathTokens = (path: string): string[] => {
     return ["/"]
   }
   const tokens = path.split("/")
-  if (path[path.length - 1] === "/") {
-    tokens.push("/")
-  }
   return tokens.filter(token => token.length > 0)
 }
