20b8edee7ebdc6e4f9b1a5608e9c28c97eef55f2
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-10-28 17:24:10 -0700

    Revert "Add supporting files"
    This reverts commit 3288eab2c0ea8a9b30d548117d433c55393ceb43.

--- create.sh
+++
@@ -1,6 +0,0 @@
-kind create cluster --name test-deployment
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
-kubectl apply -f kube.yaml
-kubectl apply -f user.yaml
-kubectl apply -f user-spec.yaml
-kubectl -n kubernetes-dashboard create token admin-user
--- delete.sh
+++
@@ -1,1 +0,0 @@
-kind delete clusters test-deployment
