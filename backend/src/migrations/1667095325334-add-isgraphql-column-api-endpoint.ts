8f891b281daddbfe2bb57647a942a3bd0da85cbd
Nikhil Shahi <nshahi1998@gmail.com>
2022-10-29 21:19:02 -0500

    add index for isGraphQl

--- backend/src/migrations/1667095325334-add-isgraphql-column-api-endpoint.ts
+++ backend/src/migrations/1667095325334-add-isgraphql-column-api-endpoint.ts
@@ -7,9 +7,13 @@ export class addIsgraphqlColumnApiEndpoint1667095325334
     await queryRunner.query(
       `ALTER TABLE api_endpoint ADD COLUMN IF NOT EXISTS "isGraphQl" BOOLEAN NOT NULL DEFAULT FALSE`,
     )
+    await queryRunner.query(
+      `CREATE INDEX IF NOT EXISTS "isGraphQl_api_endpoint" ON "api_endpoint" ("isGraphQl")`,
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX IF EXISTS "isGraphQl_api_endpoint"`)
     await queryRunner.query(
       `ALTER TABLE api_endpoint DROP COLUMN IF EXISTS "isGraphQl"`,
     )
