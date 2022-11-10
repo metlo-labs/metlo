408ca262a32fc81f159d851c2e05850eb1bf3227
Ninad Sinha <ninad@metlo.com>
2022-11-10 14:47:50 +0530

    Remove ECS type

--- cli/src/aws/types.ts
+++ cli/src/aws/types.ts
@@ -1,7 +1,6 @@
 export enum AWS_SOURCE_TYPE {
   INSTANCE = "instance",
-  NETWORK_INTERFACE = "network-interface",
-  ECS = "elastic-container-service"
+  NETWORK_INTERFACE = "network-interface"
 }
 
 export enum Protocols {
