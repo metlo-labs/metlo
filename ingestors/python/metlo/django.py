f946355c511f385c903027e6406a68362f7cba10
Ninad Sinha <ninad@metlo.com>
2022-11-11 22:43:43 +0530

    Catch errors wherever possible

--- ingestors/python/metlo/django.py
+++ ingestors/python/metlo/django.py
@@ -10,7 +10,10 @@ endpoint = "api/v1/log-request/single"
 
 class MetloDjango(object):
     def perform_request(self, data):
-        urlopen(url=self.saved_request, data=json.dumps(data).encode("utf-8"))
+        try:
+            urlopen(url=self.saved_request, data=json.dumps(data).encode("utf-8"))
+        except Exception as e:
+            print(e)
 
     def __init__(self, get_response):
         """
@@ -22,11 +25,14 @@ class MetloDjango(object):
             max_workers=settings.METLO_CONFIG.get("workers", 4)
         )
 
+        if settings.METLO_CONFIG.get("DISABLED"):
+            self.disabled = true
+
         assert (
-            settings.METLO_CONFIG.get("METLO_HOST") is not None
+                settings.METLO_CONFIG.get("METLO_HOST") is not None
         ), "METLO_CONFIG is missing METLO_HOST attribute"
         assert (
-            settings.METLO_CONFIG.get("API_KEY") is not None
+                settings.METLO_CONFIG.get("API_KEY") is not None
         ), "METLO_CONFIG is missing API_KEY attribute"
         assert urlparse(settings.METLO_CONFIG.get("METLO_HOST")).scheme in [
             "http",
@@ -47,60 +53,64 @@ class MetloDjango(object):
 
     def __call__(self, request):
         response = self.get_response(request)
-        params = request.GET if request.method == "GET" else request.POST
-        dest_ip = (
-            request.META.get("SERVER_NAME")
-            if "1.0.0.127.in-addr.arpa" not in request.META.get("SERVER_NAME")
-            else "localhost"
-        )
-        src_ip = (
-            request.META.get("REMOTE_ADDR")
-            if "1.0.0.127.in-addr.arpa" not in request.META.get("REMOTE_ADDR")
-            else "localhost"
-        )
-        source_port = request.environ["wsgi.input"].stream.raw._sock.getpeername()[1]
-        res_body = response.content.decode("utf-8")
-        data = {
-            "request": {
-                "url": {
-                    "host": request._current_scheme_host
-                    if request._current_scheme_host
-                    else src_ip,
-                    "path": request.path,
-                    "parameters": list(
-                        map(lambda x: {"name": x[0], "value": x[1]}, params.items())
-                    ),
-                },
-                "headers": list(
-                    map(
-                        lambda x: {"name": x[0], "value": x[1]}, request.headers.items()
-                    )
-                ),
-                "body": request.body.decode("utf-8"),
-                "method": request.method,
-            },
-            "response": {
-                "url": f"{dest_ip}:{request.META.get('SERVER_PORT')}",
-                "status": response.status_code,
-                "headers": list(
-                    map(
-                        lambda x: {"name": x[0], "value": x[1]},
-                        response.headers.items(),
-                    )
-                ),
-                "body": res_body,
-            },
-            "meta": {
-                "environment": "production",
-                "incoming": True,
-                "source": src_ip,
-                "sourcePort": source_port,
-                "destination": dest_ip,
-                "destinationPort": request.META.get("SERVER_PORT"),
-                "metloSource": "python/django",
-            },
-        }
-        self.pool.submit(self.perform_request, data=data)
+        if self.disabled:
+            try:
+                params = request.GET if request.method == "GET" else request.POST
+                dest_ip = (
+                    request.META.get("SERVER_NAME")
+                    if "1.0.0.127.in-addr.arpa" not in request.META.get("SERVER_NAME")
+                    else "localhost"
+                )
+                src_ip = (
+                    request.META.get("REMOTE_ADDR")
+                    if "1.0.0.127.in-addr.arpa" not in request.META.get("REMOTE_ADDR")
+                    else "localhost"
+                )
+                source_port = request.environ["wsgi.input"].stream.raw._sock.getpeername()[1]
+                res_body = response.content.decode("utf-8")
+                data = {
+                    "request": {
+                        "url": {
+                            "host": request._current_scheme_host
+                            if request._current_scheme_host
+                            else src_ip,
+                            "path": request.path,
+                            "parameters": list(
+                                map(lambda x: {"name": x[0], "value": x[1]}, params.items())
+                            ),
+                        },
+                        "headers": list(
+                            map(
+                                lambda x: {"name": x[0], "value": x[1]}, request.headers.items()
+                            )
+                        ),
+                        "body": request.body.decode("utf-8"),
+                        "method": request.method,
+                    },
+                    "response": {
+                        "url": f"{dest_ip}:{request.META.get('SERVER_PORT')}",
+                        "status": response.status_code,
+                        "headers": list(
+                            map(
+                                lambda x: {"name": x[0], "value": x[1]},
+                                response.headers.items(),
+                            )
+                        ),
+                        "body": res_body,
+                    },
+                    "meta": {
+                        "environment": "production",
+                        "incoming": True,
+                        "source": src_ip,
+                        "sourcePort": source_port,
+                        "destination": dest_ip,
+                        "destinationPort": request.META.get("SERVER_PORT"),
+                        "metloSource": "python/django",
+                    },
+                }
+                self.pool.submit(self.perform_request, data=data)
+            except Exception as e:
+                print(e)
         return response
 
     def process_exception(self, request, exception):
--- ingestors/python/metlo/flask.py
+++ ingestors/python/metlo/flask.py
@@ -10,7 +10,10 @@ endpoint = "api/v1/log-request/single"
 
 class MetloFlask:
     def perform_request(self, data):
-        urlopen(url=self.saved_request, data=json.dumps(data).encode("utf-8"))
+        try:
+            urlopen(url=self.saved_request, data=json.dumps(data).encode("utf-8"))
+        except Exception as e:
+            print(e)
 
     def __init__(self, app, metlo_host: str, metlo_api_key: str, **kwargs):
         """
@@ -21,12 +24,13 @@ class MetloFlask:
         """
         self.app = app
         self.pool = ThreadPoolExecutor(max_workers=kwargs.get("workers", 4))
+        self.disabled = kwargs.get("disabled", false)
 
         assert (
-            metlo_host is not None
+                metlo_host is not None
         ), "Metlo for FLASK __init__ is missing metlo_host parameter"
         assert (
-            metlo_api_key is not None
+                metlo_api_key is not None
         ), "Metlo for FLASK __init__ is missing metlo_api_key parameter"
         assert urlparse(metlo_host).scheme in [
             "http",
@@ -45,55 +49,59 @@ class MetloFlask:
             method="POST",
         )
 
-        @app.after_request
-        def function(response, *args, **kwargs):
-            dst_host = (
-                request.environ.get("HTTP_HOST")
-                or request.environ.get("HTTP_X_FORWARDED_FOR")
-                or request.environ.get("REMOTE_ADDR")
-            )
-            data = {
-                "request": {
-                    "url": {
-                        "host": dst_host,
-                        "path": request.path,
-                        "parameters": list(
-                            map(
-                                lambda x: {"name": x[0], "value": x[1]},
-                                request.args.items(),
-                            )
-                        ),
-                    },
-                    "headers": list(
-                        map(
-                            lambda x: {"name": x[0], "value": x[1]},
-                            (request.headers).items(),
-                        )
-                    ),
-                    "body": request.data.decode("utf-8"),
-                    "method": request.method,
-                },
-                "response": {
-                    "url": f"{request.environ.get('SERVER_NAME')}:{request.environ.get('SERVER_PORT')}",
-                    "status": response.status_code,
-                    "headers": list(
-                        map(
-                            lambda x: {"name": x[0], "value": x[1]},
-                            (response.headers).items(),
-                        )
-                    ),
-                    "body": response.data.decode("utf-8"),
-                },
-                "meta": {
-                    "environment": "production",
-                    "incoming": True,
-                    "source": request.environ.get("HTTP_X_FORWARDED_FOR")
-                    or request.environ.get("REMOTE_ADDR"),
-                    "sourcePort": request.environ.get("REMOTE_PORT"),
-                    "destination": request.environ.get("SERVER_NAME"),
-                    "destinationPort": request.environ.get("SERVER_PORT"),
-                    "metloSource": "python/flask",
-                },
-            }
-            self.pool.submit(self.perform_request, data=data)
-            return response
+        if not self.disabled:
+            @app.after_request
+            def function(response, *args, **kwargs):
+                try:
+                    dst_host = (
+                            request.environ.get("HTTP_HOST")
+                            or request.environ.get("HTTP_X_FORWARDED_FOR")
+                            or request.environ.get("REMOTE_ADDR")
+                    )
+                    data = {
+                        "request": {
+                            "url": {
+                                "host": dst_host,
+                                "path": request.path,
+                                "parameters": list(
+                                    map(
+                                        lambda x: {"name": x[0], "value": x[1]},
+                                        request.args.items(),
+                                    )
+                                ),
+                            },
+                            "headers": list(
+                                map(
+                                    lambda x: {"name": x[0], "value": x[1]},
+                                    (request.headers).items(),
+                                )
+                            ),
+                            "body": request.data.decode("utf-8"),
+                            "method": request.method,
+                        },
+                        "response": {
+                            "url": f"{request.environ.get('SERVER_NAME')}:{request.environ.get('SERVER_PORT')}",
+                            "status": response.status_code,
+                            "headers": list(
+                                map(
+                                    lambda x: {"name": x[0], "value": x[1]},
+                                    (response.headers).items(),
+                                )
+                            ),
+                            "body": response.data.decode("utf-8"),
+                        },
+                        "meta": {
+                            "environment": "production",
+                            "incoming": True,
+                            "source": request.environ.get("HTTP_X_FORWARDED_FOR")
+                                      or request.environ.get("REMOTE_ADDR"),
+                            "sourcePort": request.environ.get("REMOTE_PORT"),
+                            "destination": request.environ.get("SERVER_NAME"),
+                            "destinationPort": request.environ.get("SERVER_PORT"),
+                            "metloSource": "python/flask",
+                        },
+                    }
+                    self.pool.submit(self.perform_request, data=data)
+                except Exception as e:
+                    print(e)
+                return response
