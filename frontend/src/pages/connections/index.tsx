b4a7a9efea01b20404f4d54cc038caa425bb8c70
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-11-14 20:22:56 -0800

    Update in app docs (#108)

--- frontend/src/pages/connections/index.tsx
+++ frontend/src/pages/connections/index.tsx
@@ -1,21 +1,10 @@
-import superjson from "superjson"
 import { Heading, VStack } from "@chakra-ui/react"
-import { GetServerSideProps } from "next"
 import { SideNavLinkDestination } from "components/Sidebar/NavLinkUtils"
 import { SidebarLayoutShell } from "components/SidebarLayoutShell"
 import { ContentContainer } from "components/utils/ContentContainer"
-import ConnectionList from "components/ConnectionList"
-import { ConnectionInfo } from "@common/types"
-import axios from "axios"
-import { getAPIURL } from "~/constants"
-import { useState } from "react"
 import ConnectionDocsList from "components/ConnectionDocs"
 
-const Connections = ({ connections: _connections }) => {
-  const [connections, setConnections] = useState(
-    superjson.parse<ConnectionInfo[]>(_connections),
-  )
-
+const Connections = () => {
   return (
     <SidebarLayoutShell
       title="Connections"
@@ -27,31 +16,10 @@ const Connections = ({ connections: _connections }) => {
             Connections
           </Heading>
           <ConnectionDocsList />
-          {/* <ConnectionList
-            connections={connections}
-            setConnections={setConnections}
-          /> */}
         </VStack>
       </ContentContainer>
     </SidebarLayoutShell>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async context => {
-  let resp = await axios.get<Array<ConnectionInfo>>(
-    `${getAPIURL()}/list_connections`,
-  )
-  return {
-    props: {
-      connections: superjson.stringify(
-        resp.data.map(v => {
-          v.createdAt = new Date(v.createdAt)
-          v.updatedAt = new Date(v.updatedAt)
-          return v
-        }),
-      ),
-    },
-  }
 }
 
 export default Connections
