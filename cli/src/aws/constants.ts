282028563ebc0eaf9219d263eede66dbb68f4f41
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-11-15 01:16:43 -0800

    add more regions

--- cli/src/aws/constants.ts
+++ cli/src/aws/constants.ts
@@ -3,11 +3,19 @@ export const AWS_REGIONS = [
   "us-west-2",
   "us-east-1",
   "us-east-2",
+  "ap-south-1",
+  "ap-northeast-3",
+  "ap-northeast-2",
+  "ap-southeast-1",
+  "ap-southeast-2",
+  "ap-northeast-1",
+  "ca-central-1",
   "eu-central-1",
   "eu-west-1",
   "eu-west-2",
   "eu-west-3",
   "eu-north-1",
+  "sa-east-1",
 ]
 
 export const SUPPORTED_AWS_INSTANCES = [
