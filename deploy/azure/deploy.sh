3e96c1d40e34b22128a4c64cbfc989b5dde4fd6e
Ninad Sinha <32181677+AHarmlessPyro@users.noreply.github.com>
2022-11-16 01:15:26 +0530

    Create metlo folder in /opt

--- deploy/azure/deploy.sh
+++ deploy/azure/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mkdir /opt/metlo
+touch /opt/metlo/credentials
 echo "METLO_ADDR=<METLO_ADDRESS>:8081" >> /opt/metlo/credentials
 echo "METLO_KEY=<METLO_KEY>" >>  /opt/metlo/credentials
