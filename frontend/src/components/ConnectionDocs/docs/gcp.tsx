c7e6a58008d2b1ed0930774ad565b6af78ab7fe9
Ninad Sinha <ninad@metlo.com>
2022-11-17 00:54:39 +0530

    Update gcp docs

--- frontend/src/components/ConnectionDocs/docs/gcp.tsx
+++ frontend/src/components/ConnectionDocs/docs/gcp.tsx
@@ -4,55 +4,13 @@ const GCPDocs = () => {
   return (
     <>
       <VStack w={"full"} spacing={6}>
-        <ListNumber num={1} title="Open Ports">
-          <Text>
-            Open Port 8081 on your Metlo instance so you can start collecting
-            traffic data. It should be open to any machines you want to collect
-            traffic from.
-          </Text>
-        </ListNumber>
-        <ListNumber num={2} title="Deploy a Metlo Mirroring Instance">
-          <Text>Run the following command to spin up Metlo in GCP:</Text>
-          <Code w={"full"} p={2}>
-            <VStack>
-              <Box w={"full"}>
-                $ export PROJECT_ID={'"<'}YOUR_PROJECT_ID{'>"'}
-              </Box>
-              <Box w={"full"}>
-                $ gcloud compute instances create metlo-api-security
-                --image-family=metlo-api-security --image-project=metlo-security
-                --project=$PROJECT_ID --machine-type e2-standard-2
-              </Box>
-            </VStack>
-          </Code>
-          <Text>
-            Once you&apos;ve launched your instance run the following in the
-            instance to start Metlo:
-          </Text>
-          <Code w={"full"} p={2}>
-            $ sudo metlo start
-          </Code>
-        </ListNumber>
-        <ListNumber num={3} title="Account Permissions">
-          <Text>
-            Metlo mirroring on GCP requires a service account with the following
-            permissions:
-          </Text>
-          <Text>
-            Currently, we require these permissions for the service account:
-          </Text>
-          <Text>&nbsp;-&nbsp;Compute Admin</Text>
-          <Text>&nbsp;-&nbsp;Compute packet mirroring admin</Text>
-          <Text>&nbsp;-&nbsp;Compute packet mirroring user</Text>
-          <Text>&nbsp;-&nbsp;IAP-secured Tunnel User</Text>
-        </ListNumber>
-        <ListNumber num={4} title="Install Metlo's CLI Tool">
+      <ListNumber num={1} title="Install Metlo's CLI Tool">
           <Text>You can install metlo from npm by running the following:</Text>
           <Code w={"full"} p={2}>
             $ npm i -g @metlo/cli
           </Code>
         </ListNumber>
-        <ListNumber num={5} title="Set up Traffic Mirroring">
+        <ListNumber num={2} title="Set up Traffic Mirroring">
           <Text>To set up traffic mirroring run the following:</Text>
           <Code w={"full"} p={2}>
             <VStack>
