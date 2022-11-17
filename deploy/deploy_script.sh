63a16cbcc0bc1db48262759b4c16db1852afdbc9
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-11-17 01:45:03 -0800

    test

--- deploy/deploy_script.sh
+++ deploy/deploy_script.sh
@@ -18,7 +18,7 @@ fi
 sudo docker service start
 
 sudo mkdir /opt/metlo 
-sudo wget https://raw.githubusercontent.com/metlo-labs/metlo/master/manage-deployment.py -O /opt/metlo/manage-deployment.py
+sudo wget https://raw.githubusercontent.com/metlo-labs/metlo/test-old-python-azure/manage-deployment.py -O /opt/metlo/manage-deployment.py
 sudo chmod +x /opt/metlo/manage-deployment.py
 
 sudo ln -s /opt/metlo/manage-deployment.py /usr/bin/metlo-deploy
