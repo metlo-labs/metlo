b4755ca1b7c9aca5f3a2508dd7e1bfd397948b38
Ninad Sinha <ninad@metlo.com>
2022-12-04 00:41:29 +0530

    Store files in /home/suricata

--- deploy/suricata/install.sh
+++ deploy/suricata/install.sh
@@ -10,13 +10,13 @@ sudo touch /opt/metlo/credentials
 echo 'METLO_ADDR=$METLO_ADDR' >> opt/metlo/credentials
 echo 'METLO_KEY=$METLO_KEY' >> opt/metlo/credentials
 
-mkdir -p suricata
-cd suricata
 
 sudo apt update -y
 sudo apt install wget -y
 
-sudo wget https://raw.githubusercontent.com/metlo-labs/metlo/mirror_install_script/deploy/suricata/install.sh
+cd /home/$WHOAMI
+mkdir -p /home/$WHOAMI/suricata
+
 sudo wget https://raw.githubusercontent.com/metlo-labs/metlo/mirror_install_script/deploy/suricata/install_deps.sh
 sudo wget https://raw.githubusercontent.com/metlo-labs/metlo/mirror_install_script/deploy/suricata/install_nvm.sh
 sudo wget https://raw.githubusercontent.com/metlo-labs/metlo/mirror_install_script/deploy/suricata/local.rules
@@ -27,5 +27,4 @@ chmod +x suricata/install_nvm.sh
 chmod +x suricata/install_deps.sh
 
 sudo -E suricata/install_nvm.sh
-echo $PATH
 sudo PATH=$PATH -E suricata/install_deps.sh
