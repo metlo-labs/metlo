0f6a63a61c3c39ecb9ab9e6063f350b3d1f5b5f9
Ninad Sinha <ninad@metlo.com>
2022-12-04 09:52:13 +0530

    export to /opt/metlo/credentials properly

--- deploy/suricata/install.sh
+++ deploy/suricata/install.sh
@@ -9,8 +9,8 @@ export WHOAMI=suricata
 sudo mkdir -p /opt/metlo
 sudo touch /opt/metlo/credentials
 
-echo 'METLO_ADDR=$METLO_ADDR' >> /opt/metlo/credentials
-echo 'METLO_KEY=$METLO_KEY' >> /opt/metlo/credentials
+echo 'METLO_ADDR=${METLO_ADDR}' >> /opt/metlo/credentials
+echo 'METLO_KEY=${METLO_KEY}' >> /opt/metlo/credentials
 
 
 sudo apt update -y
