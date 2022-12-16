29f88d1745d2d6a798b964fd2277ca4241a70266
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-12-15 17:27:45 -0800

    Remove Old Suricata Stuff (#195)

--- deploy/suricata/install_nvm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-echo "Installing node and yarn"
-source /home/$WHOAMI/.nvm/nvm.sh
-nvm install 17.9.1
-nvm use 17.9.1
-npm install -g yarn
