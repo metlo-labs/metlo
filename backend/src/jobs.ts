5f361992531e2cb7d612fbe70ab1d2e1fbe2a9f0
Nikhil Shahi <nshahi1998@gmail.com>
2022-12-19 21:34:24 -0600

    regnerate specs every 60th minute

--- backend/src/jobs.ts
+++ backend/src/jobs.ts
@@ -34,7 +34,7 @@ const main = async () => {
   const logAggregateStatsSem = semaphore(1)
   const checkForUnauthenticatedSem = semaphore(1)
 
-  schedule.scheduleJob("*/10 * * * *", () => {
+  schedule.scheduleJob("*/60 * * * *", () => {
     generateSpecSem.take(async () => {
       log("Generating OpenAPI Spec Files...", true)
       await generateOpenApiSpec(ctx)
