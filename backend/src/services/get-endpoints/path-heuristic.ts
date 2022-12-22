8f9fabfe9d13457791c88d44fceacd26fffd3e32
Nikhil Shahi <nshahi1998@gmail.com>
2022-12-21 20:06:11 -0600

    add edit paths UI, add logic for creating and deleting endpoints with path list (#208)

--- backend/src/services/get-endpoints/path-heuristic.ts
+++ backend/src/services/get-endpoints/path-heuristic.ts
@@ -1,10 +1,24 @@
+import { Brackets, QueryRunner, Raw } from "typeorm"
+import { v4 as uuidv4 } from "uuid"
+import { RestMethod, RiskScore } from "@common/enums"
 import { getPathTokens } from "@common/utils"
-import { ApiTrace } from "models"
-import { getRepository } from "services/database/utils"
+import { AppDataSource } from "data-source"
+import Error400BadRequest from "errors/error-400-bad-request"
+import { ApiEndpoint, ApiTrace } from "models"
+import { getEntityManager, getQB, getRepository } from "services/database/utils"
 import { MetloContext } from "types"
+import { endpointAddNumberParams, getPathRegex, getValidPath } from "utils"
+import Error404NotFound from "errors/error-404-not-found"
+import { GetEndpointsService } from "."
+import Error500InternalServer from "errors/error-500-internal-server"
 
 const TRACE_LIMIT = 10_000
 const THRESHOLD = 0.1
+
+interface EndpointsMap {
+  endpoint: ApiEndpoint
+  similarEndpoints: Record<string, ApiEndpoint>
+}
 
 const getCounterMap = (traces: ApiTrace[]) => {
   const counterMap = {}
@@ -61,7 +75,7 @@ export const getTopSuggestedPaths = async (
   ctx: MetloContext,
   endpointId: string,
 ): Promise<string[]> => {
-  const traces = await getRepository(ctx, ApiTrace).findAndCount({
+  const traces = await getRepository(ctx, ApiTrace).find({
     select: {
       path: true,
     },
@@ -73,10 +87,160 @@ export const getTopSuggestedPaths = async (
       createdAt: "DESC",
     },
   })
-  const counterMap = getCounterMap(traces[0])
-  const distinctPaths = getDistinctPaths(counterMap, traces[0], traces[1])
+  const numTraces = traces.length
+  const counterMap = getCounterMap(traces)
+  const distinctPaths = getDistinctPaths(counterMap, traces, numTraces)
   const sorted = Object.keys(distinctPaths).sort(
     (a, b) => distinctPaths[b] - distinctPaths[a],
   )
   return sorted.slice(0, 100)
+}
+
+export const updatePaths = async (
+  ctx: MetloContext,
+  providedPaths: string[],
+  endpointId: string,
+) => {
+  if (!providedPaths || !Array.isArray(providedPaths)) {
+    throw new Error400BadRequest("Must provide list of paths to create.")
+  }
+  const queryRunner = AppDataSource.createQueryRunner()
+  try {
+    await queryRunner.connect()
+    const endpoint: ApiEndpoint = await getQB(ctx, queryRunner)
+      .from(ApiEndpoint, "endpoint")
+      .andWhere("uuid = :id", { id: endpointId })
+      .getRawOne()
+    if (!endpoint) {
+      throw new Error404NotFound("Endpoint not found.")
+    }
+    const endpointsMap = await generateEndpointsMap(
+      ctx,
+      providedPaths,
+      endpoint.method,
+      endpoint.host,
+      queryRunner,
+      getPathTokens(endpoint.path).length,
+    )
+    await queryRunner.startTransaction()
+    await updateEndpointsFromMap(ctx, endpointsMap, queryRunner)
+    await queryRunner.commitTransaction()
+  } catch (err) {
+    if (queryRunner.isTransactionActive) {
+      await queryRunner.rollbackTransaction()
+    }
+    throw err
+  } finally {
+    await queryRunner.release()
+  }
+}
+
+export const updateEndpointsFromMap = async (
+  ctx: MetloContext,
+  endpointsMap: Record<string, EndpointsMap>,
+  queryRunner: QueryRunner,
+) => {
+  for (const item of Object.values(endpointsMap)) {
+    item.endpoint.riskScore = RiskScore.NONE
+    await getEntityManager(ctx, queryRunner).save(item.endpoint)
+    const similarEndpointUuids = Object.values(item.similarEndpoints).map(
+      e => e.uuid,
+    )
+    if (similarEndpointUuids.length > 0) {
+      await GetEndpointsService.deleteEndpointsBatch(
+        ctx,
+        similarEndpointUuids,
+        queryRunner,
+      )
+    }
+  }
+}
+
+export const generateEndpointsMap = async (
+  ctx: MetloContext,
+  providedPaths: string[],
+  method: RestMethod,
+  host: string,
+  queryRunner: QueryRunner,
+  numTokens: number,
+) => {
+  const endpointsMap: Record<string, EndpointsMap> = {}
+  const set = new Set<string>()
+  for (const item of providedPaths) {
+    const validPath = getValidPath(item, numTokens)
+    if (!validPath.isValid) {
+      throw new Error400BadRequest(
+        `${item ? `${item}:` : ""} ${validPath.errMsg}`,
+      )
+    }
+    let apiEndpoint = await getEntityManager(ctx, queryRunner).findOne(
+      ApiEndpoint,
+      {
+        where: {
+          path: validPath.path,
+          method,
+          host,
+        },
+      },
+    )
+    if (apiEndpoint) {
+      throw new Error400BadRequest(
+        `${item}: An endpoint with this path already exists for this method and host.`,
+      )
+    }
+    set.add(validPath.path)
+  }
+
+  for (const path of [...set]) {
+    const pathRegex = getPathRegex(path)
+    const apiEndpoint = new ApiEndpoint()
+    apiEndpoint.uuid = uuidv4()
+    apiEndpoint.path = path
+    apiEndpoint.pathRegex = pathRegex
+    apiEndpoint.method = method
+    apiEndpoint.host = host
+    endpointAddNumberParams(apiEndpoint)
+    endpointsMap[apiEndpoint.uuid] = {
+      endpoint: apiEndpoint,
+      similarEndpoints: {},
+    }
+
+    const similarEndpoints = await getQB(ctx, queryRunner)
+      .from(ApiEndpoint, "endpoint")
+      .andWhere(
+        new Brackets(qb => {
+          qb.where(`:path ~ "pathRegex"`, { path }).orWhere(
+            `path ~ :pathRegex`,
+            { pathRegex },
+          )
+        }),
+      )
+      .andWhere("method = :method", { method })
+      .andWhere("host = :host", { host })
+      .getRawMany()
+    similarEndpoints.forEach(item => {
+      let exists = false
+      if (!endpointsMap[item.uuid]) {
+        Object.keys(endpointsMap).forEach(uuid => {
+          if (endpointsMap[uuid]?.similarEndpoints?.[item.uuid]) {
+            exists = true
+            if (
+              apiEndpoint.numberParams === item.numberParams ||
+              (endpointsMap[uuid].endpoint?.numberParams !==
+                item.numberParams &&
+                apiEndpoint.numberParams <
+                  endpointsMap[uuid].endpoint?.numberParams)
+            ) {
+              delete endpointsMap[uuid].similarEndpoints[item.uuid]
+              exists = false
+            }
+          }
+        })
+      }
+      if (!exists) {
+        endpointsMap[apiEndpoint.uuid].similarEndpoints[item.uuid] = item
+      }
+    })
+  }
+  return endpointsMap
 }
