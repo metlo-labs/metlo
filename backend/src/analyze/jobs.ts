5d6a8ae4c5f94b45993819f148c01cff577102de
Nikhil Shahi <nshahi1998@gmail.com>
2022-12-26 18:48:22 -0600

    fix update endpoint Ips queryRunner

--- backend/src/analyze/jobs.ts
+++ backend/src/analyze/jobs.ts
@@ -7,17 +7,21 @@ import { INTERNAL_IP_SET_KEY } from "./constants"
 
 export const updateEndpointIps = async (ctx: MetloContext) => {
   const queryRunner = AppDataSource.createQueryRunner()
-  const hostIps: { hostIps: { [key: string]: number } }[] = await getQB(
-    ctx,
-    queryRunner,
-  )
-    .select(['"hostIps"'])
-    .from(ApiEndpoint, "endpoint")
-    .execute()
-  await RedisClient.deleteKeyFromRedis(ctx, INTERNAL_IP_SET_KEY)
-  await RedisClient.addValueToSet(
-    ctx,
-    INTERNAL_IP_SET_KEY,
-    [...new Set(hostIps.map(e => Object.keys(e.hostIps)).flat())],
-  )
+  try {
+    await queryRunner.connect()
+    const hostIps: { hostIps: { [key: string]: number } }[] = await getQB(
+      ctx,
+      queryRunner,
+    )
+      .select(['"hostIps"'])
+      .from(ApiEndpoint, "endpoint")
+      .execute()
+    await RedisClient.deleteKeyFromRedis(ctx, INTERNAL_IP_SET_KEY)
+    await RedisClient.addValueToSet(ctx, INTERNAL_IP_SET_KEY, [
+      ...new Set(hostIps.map(e => Object.keys(e.hostIps)).flat()),
+    ])
+  } catch {
+  } finally {
+    await queryRunner.release()
+  }
 }
