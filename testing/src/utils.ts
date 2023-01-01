8ece5e744cd115137a94de5f04dbc1a30d7e94ee
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-01-01 13:54:43 -0800

    finish

--- testing/src/utils.ts
+++ testing/src/utils.ts
@@ -6,9 +6,19 @@ import jsdom from "jsdom"
 
 const SCRIPT_DEFAULT_TIMEOUT = 1000
 
-export const ALLOWED_DATA_TYPES = ["string", "bigint", "number", "boolean", "undefined", "null"]
+export const ALLOWED_DATA_TYPES = [
+  "string",
+  "bigint",
+  "number",
+  "boolean",
+  "undefined",
+  "null",
+]
 
-export const processEnvVars = (base: string, envVars: Record<string, string>) => {
+export const processEnvVars = (
+  base: string,
+  envVars: Record<string, string>,
+) => {
   for (let [key, value] of Object.entries(envVars)) {
     base = base.replace(`{{${key}}}`, value)
   }
@@ -16,25 +26,40 @@ export const processEnvVars = (base: string, envVars: Record<string, string>) =>
 }
 
 const createVM = (resp: AxiosResponse, ctx: Context) => {
-  const vm = new vm2.VM({ timeout: SCRIPT_DEFAULT_TIMEOUT, allowAsync: false, eval: false, wasm: false, })
+  const vm = new vm2.VM({
+    timeout: SCRIPT_DEFAULT_TIMEOUT,
+    allowAsync: false,
+    eval: false,
+    wasm: false,
+  })
   const sandboxItems = {}
   Object.entries(ctx.envVars).forEach(([k, v]) => vm.freeze(v, k))
   vm.freeze(resp, "resp")
   return vm
 }
 
-export const executeScript = (script: string, resp: AxiosResponse, ctx: Context) => {
+export const executeScript = (
+  script: string,
+  resp: AxiosResponse,
+  ctx: Context,
+) => {
   const vm = createVM(resp, ctx)
   const execResponse = vm.run(script)
   if (ALLOWED_DATA_TYPES.includes((typeof execResponse).toLowerCase())) {
     return execResponse
   } else {
-    const errMsg = `Returned invalid type of response from JS code. Required one of ${ALLOWED_DATA_TYPES.join(",")},found ${typeof execResponse}`
+    const errMsg = `Returned invalid type of response from JS code. Required one of ${ALLOWED_DATA_TYPES.join(
+      ",",
+    )},found ${typeof execResponse}`
     throw new Error(errMsg)
   }
 }
 
-export const extractFromHTML = (querySelectorKey: string, resp: AxiosResponse, ctx: Context) => {
+export const extractFromHTML = (
+  querySelectorKey: string,
+  resp: AxiosResponse,
+  ctx: Context,
+) => {
   const vm = createVM(resp, ctx)
   vm.freeze(jsdom.JSDOM.fragment, "jsdom")
   const dom = jsdom.JSDOM.fragment(resp.data)
@@ -47,21 +72,27 @@ export const extractFromHTML = (querySelectorKey: string, resp: AxiosResponse, c
   return execResponse
 }
 
-export const extractRegexp = (querySelectorKey: string, resp: AxiosResponse, ctx: Context) => {
-  const vm = createVM(resp, ctx)
-  vm.freeze(jsdom.JSDOM.fragment, "jsdom")
-  const dom = jsdom.JSDOM.fragment(resp.data)
-
-  vm.freeze(resp.data, "body")
-  const execResponse = vm.run(`
-    const dom = jsdom(body);
-    dom.querySelector("${querySelectorKey}")
-  `)
-  return execResponse
+export const extractRegexp = (
+  regexp: string,
+  resp: AxiosResponse,
+  ctx: Context,
+) => {
+  let strData = ""
+  if (typeof resp.data == "string") {
+    strData = resp.data
+  }
+  const match = strData.match(new RegExp(regexp))
+  if (match) {
+    return match[0]
+  }
+  return ""
 }
 
-export const stringReplacement = (string: string, envVars: Context["envVars"]) => {
-  const template = Handlebars.compile(string);
-  const templated = (template(envVars));
+export const stringReplacement = (
+  string: string,
+  envVars: Context["envVars"],
+) => {
+  const template = Handlebars.compile(string)
+  const templated = template(envVars)
   return templated
 }
