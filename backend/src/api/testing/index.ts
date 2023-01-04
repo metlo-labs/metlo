29779f0090dea054b8fcec73d77b41705a18dfb5
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-01-03 21:27:53 -0800

    get rid of generate test endpoint

--- backend/src/api/testing/index.ts
+++ backend/src/api/testing/index.ts
@@ -1,8 +1,6 @@
 import { Router } from "express"
-import { generateTestHandler } from "./generate-test"
 import { getGenTestEndpointHandler } from "./get-gen-test-endpoint"
 
 export default async function registerTestingRoutes(router: Router) {
-  router.get("/api/v1/generate-test", generateTestHandler)
   router.get("/api/v1/gen-test-endpoint", getGenTestEndpointHandler)
 }
