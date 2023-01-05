a6830e0e0e304ee33f6063b8e4a78bba630e1fc6
Ninad Sinha <ninad@metlo.com>
2023-01-05 21:50:42 +0530

    Relabel vars

--- testing/src/runner/utils.ts
+++ testing/src/runner/utils.ts
@@ -20,19 +20,19 @@ export function cartesian(takeProductOf: { [k: string]: Array<string> }) {
   const separatedKeys = Object.entries(takeProductOf).map(([key, entries]) => ({
     [key]: entries,
   }))
-  function c(part: { [x: string]: string }, index: number) {
+  function cartesianInner(part: { [x: string]: string }, index: number) {
     var k = Object.keys(separatedKeys[index])[0]
     separatedKeys[index][k].forEach(function (a) {
       var p = Object.assign({}, part, { [k]: a })
       if (index + 1 === separatedKeys.length) {
-        r.push(p)
+        res.push(p)
         return
       }
-      c(p, index + 1)
+      cartesianInner(p, index + 1)
     })
   }
 
-  let r: { [k: string]: string }[] = []
-  c({}, 0)
-  return r
+  let res: { [k: string]: string }[] = []
+  cartesianInner({}, 0)
+  return res
 }
