e61e82f41bfa3d5374de14216d4cc65fc29652e8
Nikhil Shahi <nshahi1998@gmail.com>
2023-01-05 17:54:26 -0600

    update migration time

--- backend/src/migrations/1672962660470-add-tracehash-column-to-data-field.ts
+++ backend/src/migrations/1672962660470-add-tracehash-column-to-data-field.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class addTracehashColumnToDataField1672816055671
+export class addTracehashColumnToDataField1672962660470
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "data_field" ADD "traceHash" jsonb NOT NULL DEFAULT '{}'`,
+      `ALTER TABLE "data_field" ADD COLUMN IF NOT EXISTS "traceHash" jsonb NOT NULL DEFAULT '{}'`,
     )
   }
 
