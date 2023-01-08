8f6f030c5de83231fea40b31f253455c18bd7a03
Nikhil Shahi <nshahi1998@gmail.com>
2023-01-07 18:19:09 -0600

    add check for string only data classes

--- backend/src/services/scanner/scan.ts
+++ backend/src/services/scanner/scan.ts
@@ -25,6 +25,15 @@ export const __DATA_CLASS_REGEX_MAP_INTERNAL__ = {
   [__DataClass_INTERNAL__.VIN]: VIN_REGEXP,
 }
 
+const STRING_ONLY_DATA_CLASSES: string[] = [
+  __DataClass_INTERNAL__.PHONE_NUMBER,
+  __DataClass_INTERNAL__.ADDRESS,
+  __DataClass_INTERNAL__.COORDINATE,
+  __DataClass_INTERNAL__.IP_ADDRESS,
+  __DataClass_INTERNAL__.EMAIL,
+  __DataClass_INTERNAL__.DOB,
+]
+
 export const scan = async (ctx: MetloContext, text: any): Promise<string[]> => {
   const res: string[] = []
   let convertedText: string
@@ -38,6 +47,12 @@ export const scan = async (ctx: MetloContext, text: any): Promise<string[]> => {
 
   DataClassInfo.forEach(({ className, regex: exp }) => {
     if (exp) {
+      if (
+        STRING_ONLY_DATA_CLASSES.includes(className) &&
+        typeof text !== "string"
+      ) {
+        return
+      }
       const match = new RegExp(exp).test(convertedText)
       if (match) {
         res.push(className)
