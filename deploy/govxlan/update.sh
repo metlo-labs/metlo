cf0e27b0387ff9842fe5dd8c0e76e63529c414c6
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-01-10 16:42:42 -0800

    add traffic mirror service to update script

--- deploy/govxlan/update.sh
+++ deploy/govxlan/update.sh
@@ -4,6 +4,8 @@ export WHOAMI=metlo
 
 echo "UPDATING"
 sudo curl -L https://github.com/metlo-labs/metlo/releases/download/v0.0.4/metlo_0.0.4_linux_amd64.tar.gz > /home/$WHOAMI/metlo.tar.gz
+sudo curl -L https://raw.githubusercontent.com/metlo-labs/metlo/master/deploy/govxlan/metlo-traffic-mirror.service > /home/$WHOAMI/metlo/metlo-traffic-mirror.service
 sudo tar -xf /home/$WHOAMI/metlo.tar.gz -C /home/$WHOAMI/metlo
 sudo cp /home/$WHOAMI/metlo/metlo-pcap /usr/local/bin
+sudo mv /home/$WHOAMI/metlo/metlo-traffic-mirror.service /lib/systemd/system/metlo-traffic-mirror.service -f
 sudo chmod +x /usr/local/bin/metlo-pcap
