a1cd4c3c037ebd214b4684b39768d1e3bd723921
Ninad Sinha <ninad@metlo.com>
2023-01-12 00:38:20 +0530

    Don't use queryrunner

--- backend/src/services/testing/env.ts
+++ backend/src/services/testing/env.ts
@@ -1,12 +1,10 @@
-import { AppDataSource } from "data-source"
 import jsyaml from "js-yaml"
 import { MetloConfig } from "models/metlo-config"
-import { getEntityManager } from "services/database/utils"
+import { getRepository } from "services/database/utils"
 import { MetloContext } from "types"
 
 export async function getGlobalEnvService(ctx: MetloContext) {
-  const qr = await AppDataSource.createQueryRunner()
-  const configRaw = await getEntityManager(ctx, qr).findOne(MetloConfig, {
+  const configRaw = await getRepository(ctx, MetloConfig).findOne({
     select: { configString: true },
     where: {},
   })
