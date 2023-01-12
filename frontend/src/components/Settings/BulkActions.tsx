2de6ba975f9bbb5145c4cdb2edcc390dff30159e
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-01-11 19:32:25 -0800

    Clear All Sensitive Datafields (#275)

---
+++ frontend/src/components/Settings/BulkActions.tsx
@@ -0,0 +1,113 @@
+import React, { useRef, useState } from "react"
+import {
+  Button,
+  VStack,
+  Heading,
+  Divider,
+  HStack,
+  useDisclosure,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  useToast,
+} from "@chakra-ui/react"
+import { SectionHeader } from "components/utils/Card"
+import { clearSensitiveData } from "api/dataFields"
+import { makeToast } from "utils"
+import { formatMetloAPIErr, MetloAPIErr } from "api/utils"
+
+const BulkActions: React.FC = React.memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
+  const [clearSensitiveDataLoading, setSensitiveDataLoading] = useState(false)
+  const cancelRef = useRef()
+
+  const clearSensitiveDataBtn = () => {
+    setSensitiveDataLoading(true)
+    clearSensitiveData()
+      .then(() => {
+        toast(
+          makeToast({
+            title: "Cleared All Sensitive Data.",
+            status: "success",
+          }),
+        )
+      })
+      .catch(err => {
+        toast(
+          makeToast({
+            title: "Failed Clearing Sensitive Data...",
+            status: "error",
+            description: formatMetloAPIErr(err.response.data as MetloAPIErr),
+          }),
+        )
+      })
+      .finally(() => {
+        setSensitiveDataLoading(false)
+        onClose()
+      })
+  }
+
+  return (
+    <VStack
+      bg="white"
+      spacing="8"
+      rounded="md"
+      w="full"
+      alignItems="flex-start"
+      borderWidth="1px"
+      p="30px"
+    >
+      <VStack w="full" spacing="1" alignItems="flex-start">
+        <Heading fontWeight="bold" fontSize="26">
+          Bulk Actions
+        </Heading>
+        <Divider pt="3" />
+        <VStack w="full" alignItems="flex-start" pt="5">
+          <SectionHeader text="Datafields" />
+          <HStack pt="1">
+            <Button onClick={onOpen} variant="delete">
+              Clear All Sensitive Data
+            </Button>
+          </HStack>
+        </VStack>
+      </VStack>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Clear All Sensitive Data
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure you want to clear all sensitive data? This is not
+              reversible but Metlo will automatically identify any new sensitive
+              data as new traces come in.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                isLoading={clearSensitiveDataLoading}
+                variant="delete"
+                onClick={clearSensitiveDataBtn}
+                ml={3}
+              >
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </VStack>
+  )
+})
+
+export default BulkActions
