8f8633d998040ef4b6140cb513c01bcc460d9927
Ninad Sinha <32181677+AHarmlessPyro@users.noreply.github.com>
2023-01-15 01:52:39 +0530

    Add template for bola admin and multi-tenant (#290)

---
+++ testing/src/templates/bola-multi-tenant.ts
@@ -0,0 +1,44 @@
+import { GenTestEndpoint } from "../generate/types"
+import { TestBuilder, TestStepBuilder } from "../generate/builder"
+import { AssertionType } from "../types/enums"
+
+export default {
+  name: "BOLA_MULTI_TENANT",
+  version: 1,
+  builder: (endpoint: GenTestEndpoint) => {
+    if (!endpoint.authConfig) {
+      throw new Error(`No auth config defined for host: "${endpoint.host}"...`)
+    }
+
+    return new TestBuilder()
+      .setMeta({
+        name: `${endpoint.path} BOLA_MULTI_TENANT`,
+        severity: "HIGH",
+        tags: ["BOLA", "MULTI-TENANT"],
+      })
+      .addTestStep(
+        TestStepBuilder.sampleRequest(endpoint, "TENANT_A").assert({
+          type: AssertionType.enum.JS,
+          value: "resp.status < 300",
+        }),
+      )
+      .addTestStep(
+        TestStepBuilder.sampleRequestWithoutAuth(endpoint, "TENANT_A")
+          .addAuth(endpoint, "TENANT_B")
+          .assert({
+            type: AssertionType.enum.EQ,
+            key: "resp.status",
+            value: [404],
+          }),
+      )
+      .addTestStep(
+        TestStepBuilder.sampleRequestWithoutAuth(endpoint, "TENANT_B")
+          .addAuth(endpoint, "TENANT_A")
+          .assert({
+            type: AssertionType.enum.EQ,
+            key: "resp.status",
+            value: [404],
+          }),
+      )
+  },
+}
--- testing/src/templates/index.ts
+++ testing/src/templates/index.ts
@@ -1,12 +1,16 @@
 import { TestTemplate } from "./types"
 import BOLAV1 from "./bola-v1"
+import BOLA_ADMINV1 from "./bola-admin-v1"
+import BOLA_MULTIV1 from "./bola-multi-tenant"
 import BrokenAuthenticationV1 from "./broken-authentication-v1"
 import GenericV1 from "./generic-v1"
 import HSTSV1 from "./hsts-v1"
 
 export const TEMPLATES: TestTemplate[] = [
   BOLAV1,
+  BOLA_ADMINV1,
+  BOLA_MULTIV1,
   BrokenAuthenticationV1,
   GenericV1,
-  HSTSV1
+  HSTSV1,
 ]
