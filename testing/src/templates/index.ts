339b35e4d3b731697f2dc304381752076ea98f4f
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-01-15 13:37:34 -0800

    rename multitenant template

--- testing/src/templates/index.ts
+++ testing/src/templates/index.ts
@@ -1,7 +1,7 @@
 import { TestTemplate } from "./types"
 import BOLAV1 from "./bola-v1"
 import BOLA_ADMINV1 from "./bola-admin-v1"
-import BOLA_MULTIV1 from "./bola-multi-tenant"
+import BOLA_MULTIV1 from "./bola-multi-tenant-v1"
 import BrokenAuthenticationV1 from "./broken-authentication-v1"
 import GenericV1 from "./generic-v1"
 import HSTSV1 from "./hsts-v1"
