3994135d3c1c12a69af5494d932b026e97322500
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-01-19 15:46:26 -0800

    fix

--- testing/src/runner/assertions.ts
+++ testing/src/runner/assertions.ts
@@ -24,42 +24,38 @@ export const runAssertion = (
         assertion.value as string,
         ctx.envVars,
       )
-      assertionTruth = !!executeScript(assertionValue, response, ctx)
+      return !!executeScript(assertionValue, response, ctx)
     } else {
       throw new Error(
         `Script must be of type string. Received script of type ${typeof assertion.value}`,
       )
     }
-  } else if (
-    assertion.type == AssertionType.enum.EQ ||
-    assertion.type == AssertionType.enum.REGEXP
-  ) {
-    if (!assertion.key) {
-      throw new Error("Must specify a key for assertion")
-    }
-    let assertionKey = undefined
-    let assertionValue = undefined
-    if ((typeof assertion.key).toLowerCase() == "string") {
-      assertionKey = stringReplacement(assertion.key as string, ctx.envVars)
+  }
+  if (!assertion.key) {
+    throw new Error("Must specify a key for assertion")
+  }
+  let assertionKey = undefined
+  let assertionValue = undefined
+  if ((typeof assertion.key).toLowerCase() == "string") {
+    assertionKey = stringReplacement(assertion.key as string, ctx.envVars)
+  } else {
+    assertionKey = assertion.key
+  }
+  if ((typeof assertion.value).toLowerCase() == "string") {
+    assertionValue = stringReplacement(assertion.value as string, ctx.envVars)
+  } else {
+    assertionValue = assertion.value
+  }
+  const currentValue = getKeyValue(assertionKey, response, ctx)
+  if (assertion.type == AssertionType.enum.EQ) {
+    if (assertionValue instanceof Array) {
+      assertionTruth = assertionValue.some(e => e == currentValue)
     } else {
-      assertionKey = assertion.key
+      assertionTruth = assertionValue === currentValue
     }
-    if ((typeof assertion.value).toLowerCase() == "string") {
-      assertionValue = stringReplacement(assertion.value as string, ctx.envVars)
-    } else {
-      assertionValue = assertion.value
-    }
-    const currentValue = getKeyValue(assertionKey, response, ctx)
-    if (assertion.type == AssertionType.enum.EQ) {
-      if (assertionValue instanceof Array) {
-        assertionTruth = assertionValue.some(e => e == currentValue)
-      } else {
-        assertionTruth = assertionValue === currentValue
-      }
-    } else if (assertion.type == AssertionType.enum.REGEXP) {
-      const regex = new RegExp(assertionValue as string)
-      assertionTruth = regex.test(currentValue)
-    }
+  } else if (assertion.type == AssertionType.enum.REGEXP) {
+    const regex = new RegExp(assertionValue as string)
+    assertionTruth = regex.test(currentValue)
   }
   return assertionTruth
 }
