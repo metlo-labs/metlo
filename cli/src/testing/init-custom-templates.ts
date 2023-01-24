9f421f4bce82d9fef023f4c0b3544a429f5328a1
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-01-24 14:08:26 -0800

    style

--- cli/src/testing/init-custom-templates.ts
+++ cli/src/testing/init-custom-templates.ts
@@ -1,26 +1,41 @@
 import fs from "fs"
-import { execSync } from "child_process"
+import { promisify } from "util"
+import child_process from "child_process"
 
 import chalk from "chalk"
 import commandExists from "command-exists"
+import ora from "ora"
 
-export const initCustomTemplates = (path: string) => {
+const exists = promisify(fs.exists)
+const mkdir = promisify(fs.mkdir)
+const exec = promisify(child_process.exec)
+
+export const initCustomTemplates = async (path: string) => {
+  const spinner = ora()
+  spinner.start(chalk.dim("Initializing custom template project..."))
   const nodeVersion = process.version
   const majorVersion = parseInt(nodeVersion.match(/v([\d]+)./)[1])
   if (majorVersion < 16) {
-    console.log(
+    spinner.fail(
       chalk.bold.red(
         "Node 16+ is required for Metlo. We recommend using nvm to install Node: https://github.com/nvm-sh/nvm",
       ),
     )
     return
   }
-  if (!commandExists.sync("npm")) {
-    console.log(chalk.bold.red("Error: npm not installed"))
+  const npmExists = await promisify(commandExists)("npm")
+  if (!npmExists) {
+    spinner.fail(chalk.bold.red("Error: npm not installed"))
     return
   }
-  fs.mkdirSync(path)
-  fs.mkdirSync(`${path}/templates`)
-  execSync("npm init -y", { cwd: path })
-  execSync("npm install @metlo/testing", { cwd: path })
+  const pathExists = await exists(path)
+  if (pathExists) {
+    spinner.fail(chalk.bold.red(`Path: "${path}" already exists`))
+    return
+  }
+  await mkdir(path)
+  await mkdir(`${path}/templates`)
+  await exec("npm init -y", { cwd: path })
+  await exec("npm install @metlo/testing", { cwd: path })
+  spinner.succeed(`Created custom template project at "${path}".`)
 }
