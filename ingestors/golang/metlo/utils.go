42493659afd537ef8ef675307ab60e165cbd6539
Ninad Sinha <ninad@metlo.com>
2023-02-06 01:25:55 +0530

    Return after error

--- ingestors/golang/metlo/utils.go
+++ ingestors/golang/metlo/utils.go
@@ -15,14 +15,19 @@ func TestInit(host string, apiKey string) {
 	res, err := client.Do(req)
 	if err != nil {
 		Log.Error(err)
+		return
 	}
 	if res.StatusCode == 404 {
 		Log.Errorf("Metlo host at %s is unreachable.\n"+
 			"Metlo host may be incorrect or metlo version may be old", host)
+		return
 	} else if res.StatusCode == 401 {
 		Log.Error("Could not validate Metlo API key.")
+		return
 	} else if res.StatusCode != 200 {
 		Log.Errorf("Problem encountered while validating connection to Metlo.\n"+
 			"Received error code %d. Enabling by default", res.StatusCode)
+		return
 	}
+	return
 }
