2669311bcd84beb134f3240238a41ce2024aed2a
Nikhil Shahi <nshahi1998@gmail.com>
2023-02-22 00:50:59 -0600

    remove time logs

--- ingestors/rust-common/src/lib.rs
+++ ingestors/rust-common/src/lib.rs
@@ -7,7 +7,6 @@ use mappers::{map_ingest_api_trace, map_process_trace_res};
 use process_trace::process_api_trace;
 use std::path::Path;
 use std::sync::Arc;
-use std::time::Instant;
 use tokio::net::UnixListener;
 use tokio::sync::{RwLock, Semaphore, TryAcquireError};
 use tokio_stream::wrappers::UnixListenerStream;
@@ -119,12 +118,8 @@ impl MetloIngest for MIngestServer {
             match TASK_RUN_SEMAPHORE.try_acquire() {
                 Ok(permit) => {
                     tokio::spawn(async move {
-                        let start = Instant::now();
                         let res = process_api_trace(&mapped_api_trace);
-                        log::info!("Process Api Trace {:?}", start.elapsed());
-                        let start2 = Instant::now();
                         send_api_trace(mapped_api_trace, res).await;
-                        log::info!("Send Api Trace {:?}", start2.elapsed());
                         drop(permit);
                     });
                 }
