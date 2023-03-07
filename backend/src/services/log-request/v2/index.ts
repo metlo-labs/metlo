2745ae22edf0327bdccfe03485826ff0a7fa8bfa
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-03-07 15:16:22 -0800

    skip analyzing reqs with null chars (#413)

--- backend/src/services/log-request/v2/index.ts
+++ backend/src/services/log-request/v2/index.ts
@@ -47,6 +47,11 @@ export const logRequest = async (
       mlog.debug("Trace queue overloaded")
       return
     }
+    if (JSON.stringify(traceParams).includes("\x00")) {
+      mlog.debug("Found null char, skipping...")
+      mlog.count("collector.null_char_found")
+      return
+    }
 
     const validPath = getValidPath(traceParams?.request?.url?.path)
     if (!validPath.isValid) {
