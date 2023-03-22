173817536b9e6d3d7e06150e9dfaeaa83ffb72b3
Nikhil Shahi <nshahi1998@gmail.com>
2023-03-22 18:48:43 -0500

    update rust agent log

--- ingestors/rust-common/src/send_trace.rs
+++ ingestors/rust-common/src/send_trace.rs
@@ -381,7 +381,7 @@ pub async fn send_api_trace(trace: ApiTrace, processed_trace: (ProcessTraceRes, 
                     log::debug!("Failed to send trace: {}", msg.unwrap_or_default())
                 }
             }
-            Err(err) => log::warn!("{}", err.to_string()),
+            Err(err) => log::debug!("{}", err.to_string()),
         }
     }
     drop(conf_read)
