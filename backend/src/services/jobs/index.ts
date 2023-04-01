2cae6aeb4d56ca25cf8ebb08d016ebad122f4293
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-03-31 22:21:51 -0700

    fix

--- backend/src/services/jobs/index.ts
+++ backend/src/services/jobs/index.ts
@@ -2,12 +2,10 @@ import checkForUnauthenticatedEndpoints from "./check-unauthenticated-endpoints"
 import clearApiTraces from "./clear-api-traces"
 import generateEndpointsFromTraces from "./generate-endpoints-traces"
 import generateOpenApiSpec from "./generate-openapi-spec"
-import monitorEndpointForHSTS from "./monitor-endpoint-hsts"
 
 export {
   checkForUnauthenticatedEndpoints,
   clearApiTraces,
   generateEndpointsFromTraces,
   generateOpenApiSpec,
-  monitorEndpointForHSTS,
 }
