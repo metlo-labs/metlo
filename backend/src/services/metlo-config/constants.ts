1daec617a1b265f921d10f62a50739404e7a0db5
Nikhil Shahi <nshahi1998@gmail.com>
2023-04-04 23:43:20 -0500

    fix config schema

--- backend/src/services/metlo-config/constants.ts
+++ backend/src/services/metlo-config/constants.ts
@@ -224,24 +224,24 @@ export const METLO_CONFIG_SCHEMA = {
       },
       uniqueItems: true,
     },
-  },
-  pathBlockList: {
-    type: "array",
-    items: {
-      type: "object",
-      properties: {
-        host: {
-          type: "string",
-          minLength: 1,
-          format: "regex",
-        },
-        paths: {
-          type: "array",
-          uniqueItems: true,
-          items: {
+    pathBlockList: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          host: {
             type: "string",
             minLength: 1,
             format: "regex",
+          },
+          paths: {
+            type: "array",
+            uniqueItems: true,
+            items: {
+              type: "string",
+              minLength: 1,
+              format: "regex",
+            },
           },
         },
       },
