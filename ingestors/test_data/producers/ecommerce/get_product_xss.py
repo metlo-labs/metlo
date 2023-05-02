95faf58afbc2f973d12b8c3371226a9298cdf495
Nikhil Shahi <nshahi1998@gmail.com>
2023-05-02 01:10:48 -0500

    add attacks to test data

---
+++ ingestors/test_data/producers/ecommerce/get_product_xss.py
@@ -0,0 +1,51 @@
+from datetime import timedelta
+from uuid import uuid4
+import json
+
+from producers.ecommerce.utils import get_product, sources, destinations
+from producers.utils import get_meta, JSON_HEADER
+from producers.base import BaseProducer
+
+
+class EcommerceGetProductXSSProducer(BaseProducer):
+
+    emit_probability = 0.1
+
+    def get_data_point(self) -> dict:
+        product_uuid = str(uuid4())
+        resp_body = {
+            "success": True,
+            "product": {
+                **get_product(self.fake, product_uuid),
+                "owner": {
+                    "user_uuid": str(uuid4()),
+                    "name": self.fake.first_name(),
+                    "email": self.fake.free_email(),
+                    "address": self.fake.address(),
+                    "phoneNumber": self.get_fake_phonenum(),
+                    "dob": self.fake.date_of_birth().isoformat(),
+                    "password": self.fake.sentence(nb_words=5),
+                }
+            }
+        }
+        return {
+            "request": {
+                "url": {
+                    "host": "test-ecommerce.metlo.com",
+                    "path": f"/product/{product_uuid}",
+                    "parameters": [{
+                      "name": "query",
+                      "value": "<script>alert('XSS')</script>"
+                    }]
+                },
+                "headers": [],
+                "method": "GET",
+                "body": "",
+            },
+            "response": {
+                "status": 200,
+                "headers": [JSON_HEADER],
+                "body": json.dumps(resp_body),
+            },
+            "meta": get_meta(sources, destinations),
+        }
---
+++ ingestors/test_data/producers/ecommerce/login_sqli.py
@@ -0,0 +1,41 @@
+from datetime import timedelta
+from uuid import uuid4
+import json
+
+from producers.ecommerce.utils import sources, destinations
+from producers.utils import get_meta, JSON_HEADER
+from producers.base import BaseProducer
+
+
+class EcommerceLoginSQLIProducer(BaseProducer):
+
+    emit_probability = 1
+
+    def get_data_point(self) -> dict:
+        resp_body = {
+            "success": True,
+            "user_uuid": str(uuid4()),
+            "api_key": str(uuid4()),
+        }
+        req_body = {
+            "email": "hi' or 'x'='x';",
+            "password": self.fake.sentence(nb_words=5),
+        }
+        return {
+            "request": {
+                "url": {
+                    "host": "test-ecommerce.metlo.com",
+                    "path": "/login",
+                    "parameters": []
+                },
+                "headers": [],
+                "method": "POST",
+                "body": json.dumps(req_body),
+            },
+            "response": {
+                "status": 200,
+                "headers": [JSON_HEADER],
+                "body": json.dumps(resp_body),
+            },
+            "meta": get_meta(sources, destinations),
+        }
