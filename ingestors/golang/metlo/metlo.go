fbeff95a83340d431afd913d2608424c014892ed
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-05-28 16:54:22 +0200

    bootstrap inst async

--- ingestors/golang/metlo/metlo.go
+++ ingestors/golang/metlo/metlo.go
@@ -2,9 +2,9 @@ package metlo
 
 import (
 	"context"
+	"os"
 	"os/exec"
 	"strconv"
-	"sync"
 	"time"
 
 	pb "github.com/metlo-labs/metlo/ingestors/golang/metlo/proto"
@@ -13,8 +13,6 @@ import (
 )
 
 type metlo struct {
-	mu            sync.Mutex
-	ts            []int64
 	disable       bool
 	stream        pb.MetloIngest_ProcessTraceAsyncClient
 	streamSetup   bool
@@ -70,7 +68,6 @@ func InitMetlo(metloHost string, metloKey string) *metlo {
 
 func InitMetloCustom(metloHost string, metloKey string, rps int, backendPort int, collectorPort int, encryptionKey *string, disable bool) *metlo {
 	inst := &metlo{
-		ts:            make([]int64, 0, rps),
 		rps:           rps,
 		metloHost:     metloHost,
 		metloKey:      metloKey,
@@ -80,20 +77,24 @@ func InitMetloCustom(metloHost string, metloKey string, rps int, backendPort int
 		encryptionKey: encryptionKey,
 		streamSetup:   false,
 	}
-	agentStartErr := inst.StartLocalAgent()
+	go inst.BootstrapInstance()
+	return inst
+}
+
+func (m *metlo) BootstrapInstance() {
+	agentStartErr := m.StartLocalAgent()
 	if agentStartErr != nil {
 		logger.Println("Couldn't start metlo agent", agentStartErr)
-		inst.disable = true
-		return inst
+		m.disable = true
 	}
 	conn, err := ConnectLocalProcessAgent()
 	if err != nil {
 		logger.Println("Couldn't connect to metlo agent", err)
+		m.disable = true
 	} else {
-		inst.stream = conn
-		inst.streamSetup = true
+		m.stream = conn
+		m.streamSetup = true
 	}
-	return inst
 }
 
 func (m *metlo) StartLocalAgent() error {
@@ -109,6 +110,8 @@ func (m *metlo) StartLocalAgent() error {
 		args = append(args, "-e", *m.encryptionKey)
 	}
 	cmd := exec.Command("metlo-agent", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	err := cmd.Start()
 	if err != nil {
 		return err
@@ -135,27 +138,5 @@ func (m *metlo) Send(data MetloTrace) {
 }
 
 func (m *metlo) Allow() bool {
-	if !m.disable {
-		m.mu.Lock()
-		tmp_ts := make([]int64, 0, 10)
-		now := time.Now()
-		curr := now.UTC().UnixMilli()
-		if len(m.ts) == 0 {
-
-		} else {
-			for x := 0; x < len(m.ts); x++ {
-				if (curr - m.ts[x]) <= 1000 {
-					tmp_ts = append(tmp_ts, m.ts[x])
-				}
-			}
-		}
-		m.ts = tmp_ts
-		if len(m.ts) < MetloDefaultRPS {
-			m.ts = append(m.ts, curr)
-			m.mu.Unlock()
-			return true
-		}
-		m.mu.Unlock()
-	}
-	return false
+	return !m.disable
 }
