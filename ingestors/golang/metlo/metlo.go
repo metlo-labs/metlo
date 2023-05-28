7d1c0ac71f49d49a3dabbc81440969e73bea940f
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-05-28 00:10:04 -0500

    fix

--- ingestors/golang/metlo/metlo.go
+++ ingestors/golang/metlo/metlo.go
@@ -6,7 +6,6 @@ import (
 	"sync"
 	"time"
 
-	mi "github.com/metlo-labs/metlo/ingestors/golang/metlo/proto"
 	pb "github.com/metlo-labs/metlo/ingestors/golang/metlo/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -109,44 +108,4 @@ func (m *metlo) Allow() bool {
 		m.mu.Unlock()
 	}
 	return false
-}
-
-func MapMetloTraceToMetloIngestRPC(trace MetloTrace) mi.ApiTrace {
-	reqURLParams := make([]*mi.KeyVal, 0)
-	for _, k := range trace.Request.Url.Parameters {
-		reqURLParams = append(reqURLParams, &mi.KeyVal{Name: k.Name, Value: k.Value})
-	}
-	reqHeaders := make([]*mi.KeyVal, 0)
-	for _, k := range trace.Request.Headers {
-		reqHeaders = append(reqHeaders, &mi.KeyVal{Name: k.Name, Value: k.Value})
-	}
-	respHeaders := make([]*mi.KeyVal, 0)
-	for _, k := range trace.Response.Headers {
-		respHeaders = append(respHeaders, &mi.KeyVal{Name: k.Name, Value: k.Value})
-	}
-	return mi.ApiTrace{
-		Response: &mi.ApiResponse{
-			Status:  int32(trace.Response.Status),
-			Headers: respHeaders,
-			Body:    trace.Response.Body,
-		},
-		Request: &mi.ApiRequest{
-			Method: trace.Request.Method,
-			Url: &mi.ApiUrl{
-				Host:       trace.Request.Url.Host,
-				Path:       trace.Request.Url.Path,
-				Parameters: reqURLParams,
-			},
-			Headers: reqHeaders,
-			Body:    trace.Request.Body,
-		},
-		Meta: &mi.ApiMeta{
-			Environment:     trace.Meta.Environment,
-			Incoming:        trace.Meta.Incoming,
-			Source:          trace.Meta.Source,
-			SourcePort:      int32(trace.Meta.SourcePort),
-			Destination:     trace.Meta.Destination,
-			DestinationPort: int32(trace.Meta.DestinationPort),
-		},
-	}
 }
